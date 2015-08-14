90594a9c7c6624d4f32303507bcb3eaa7ece0e48
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-08-14 16:56:35 -0600

    Adds tests around stream meta object.

--- spec/actions/community_actions_spec.js
+++ spec/actions/community_actions_spec.js
@@ -1,4 +1,4 @@
-import { expect, isFSA } from '../spec_helper'
+import { expect, isFSA, hasStreamMetadata } from '../spec_helper'
 import * as subject from '../../src/actions/community_actions'
 import * as TYPE from '../../src/constants/action_types'
 
@@ -7,19 +7,23 @@ describe('actions', () => {
   it('#loadChannels returns the expected action', () => {
     const action = subject.loadChannels()
     expect(isFSA(action)).to.be.true
+    expect(hasStreamMetadata(action)).to.be.true
     expect(action.type).to.equal('LOAD_STREAM')
     expect(action.payload.endpoint).to.be.equal('/_data/channels.json')
     expect(action.payload.vo).to.be.empty
     expect(action.meta.mappingType).to.equal('users')
+    expect(action.meta.renderStream).to.be.a('function')
   })
 
   it('#loadAwesomePeople returns the expected action', () => {
     const action = subject.loadAwesomePeople()
     expect(isFSA(action)).to.be.true
+    expect(hasStreamMetadata(action)).to.be.true
     expect(action.type).to.equal('LOAD_STREAM')
     expect(action.payload.endpoint).to.be.equal('/_data/awesome_people.json')
     expect(action.payload.vo).to.be.empty
     expect(action.meta.mappingType).to.equal('users')
+    expect(action.meta.renderStream).to.be.a('function')
   })
 
 })
--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -5,11 +5,18 @@ export let expect = exp
 export function isFSA(action) {
   return (
     typeof action.type !== 'undefined' &&
-    Object.keys(action).every(isValidKey)
+    Object.keys(action).every(isValidFSAKey)
   )
 }
 
-function isValidKey(key) {
+export function hasStreamMetadata(action) {
+  return (
+    typeof action.meta !== 'undefined' &&
+    Object.keys(action.meta).every(isValidStreamMetaKey)
+  )
+}
+
+function isValidFSAKey(key) {
   const validKeys = [
     'type',
     'payload',
@@ -18,3 +25,12 @@ function isValidKey(key) {
   ]
   return validKeys.indexOf(key) > -1
 }
+
+function isValidStreamMetaKey(key) {
+  const validKeys = [
+    'mappingType',
+    'renderStream',
+  ]
+  return validKeys.indexOf(key) > -1
+}
+
