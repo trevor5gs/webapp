2b7ef52f58975a3fbf170412093bcb31fa9ba0f2
Matthew Kitt <mk@mkitt.net>
2015-08-21 10:09:43 -0600

    Make the Navbar less hideous

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { ElloMark } from '../iconography/ElloIcons'
 
 class Navbar extends React.Component {
   render() {
     return (
       <nav className="Navbar" role="navigation">
-        <Link to="/" className="tmp">Home</Link>
-        <Link to="/discover">Discover</Link>
-        <Link to="/search">Search</Link>
-        <Link to="/onboarding/channels">Onboarding</Link>
+        <Link to="/">
+          <ElloMark />
+        </Link>
+        <div className="NavbarLinks">
+          <Link to="/discover">Discover</Link>
+          <Link to="/search">Search</Link>
+          <Link to="/onboarding/channels">Onboarding</Link>
+        </div>
       </nav>
     )
   }
