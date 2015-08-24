34d51c115c146302e4c74b0cd2d47e623a902ae0
Matthew Kitt <mk@mkitt.net>
2015-08-24 16:22:07 -0600

    Dump redux-react-router until it’s stable.
    It’s a little bumpy with all the warnings it’s generating from React and 
    there really isn’t any movement on the package itself. It seems like the
    “rackt” group may provide their own wrapper around react-router or it 
    will be solved in that project specifically.
    
    I’m watching https://github.com/acdlite/redux-react-router/issues/7 for
    any resolution and will potentially bring it back in if that ever gets
    fixed.

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 import { Router, Route, Redirect } from 'react-router'
 import BrowserHistory from 'react-router/lib/BrowserHistory'
-import { reduxRouteComponent, routerStateReducer } from 'redux-react-router'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import * as reducers from './reducers'
@@ -20,25 +19,23 @@ const history = new BrowserHistory()
 const logger = createLogger({ collapsed: true })
 
 const createStoreWithMiddleware = applyMiddleware(thunk, requester, logger)(createStore)
-const reducer = combineReducers({ router: routerStateReducer, ...reducers })
+const reducer = combineReducers(reducers)
 const store = createStoreWithMiddleware(reducer)
 
 const element = (
   <Provider store={store}>
     {() =>
       <Router history={history}>
-        <Route component={reduxRouteComponent(store)}>
-          <Route path="/" component={App}>
-            <Route path="search" component={SearchView} />
-            <Route path="discover" component={DiscoverView} />
-            <Route path="onboarding">
-              <Route path="channels" component={ChannelPicker} />
-              <Route path="awesome-people" component={PeoplePicker} />
-              <Route path="profile-header" component={HeaderPicker} />
-              <Route path="profile-avatar" component={AvatarPicker} />
-              <Route path="profile-bio" component={BioCreator} />
-              <Redirect from="onboarding" to="channels" />
-            </Route>
+        <Route path="/" component={App}>
+          <Route path="search" component={SearchView} />
+          <Route path="discover" component={DiscoverView} />
+          <Route path="onboarding">
+            <Route path="channels" component={ChannelPicker} />
+            <Route path="awesome-people" component={PeoplePicker} />
+            <Route path="profile-header" component={HeaderPicker} />
+            <Route path="profile-avatar" component={AvatarPicker} />
+            <Route path="profile-bio" component={BioCreator} />
+            <Redirect from="onboarding" to="channels" />
           </Route>
         </Route>
       </Router>
