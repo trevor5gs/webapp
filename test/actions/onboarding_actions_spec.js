96bca0600bbb539b10ad9b20602b84ca5577f955
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-08-26 13:42:12 -0600

    Switch over to using tape instead of mocha.

--- test/actions/onboarding_actions_spec.js
+++ test/actions/onboarding_actions_spec.js
@@ -1,27 +1,27 @@
-import { expect, isFSA, hasStreamMetadata } from '../spec_helper'
+import { test, isFSA, hasStreamMetadata } from '../spec_helper'
 import * as subject from '../../src/actions/onboarding'
 
-describe('actions', () => {
-  it('#loadChannels returns the expected action', () => {
-    const action = subject.loadChannels()
-    expect(isFSA(action)).to.be.true
-    expect(hasStreamMetadata(action)).to.be.true
-    expect(action.type).to.equal('LOAD_STREAM')
-    expect(action.payload.endpoint).to.be.equal('/test/mock/data/channels.json')
-    expect(action.payload.vo).to.be.empty
-    expect(action.meta.mappingType).to.equal('users')
-    expect(action.meta.renderStream).to.be.a('function')
-  })
+test('#loadChannels', (assert) => {
+  const action = subject.loadChannels()
+  assert.true(isFSA(action), 'should contain FSA properties')
+  assert.true(hasStreamMetadata(action), 'should have stream metadata')
+  assert.equal(action.type, 'LOAD_STREAM')
+  assert.equal(action.payload.endpoint, '/test/mock/data/channels.json')
+  assert.deepEqual(action.payload.vo, {})
+  assert.equal(action.meta.mappingType, 'users')
+  assert.equal(typeof action.meta.renderStream, 'function')
+  assert.end()
+})
 
-  it('#loadAwesomePeople returns the expected action', () => {
-    const action = subject.loadAwesomePeople()
-    expect(isFSA(action)).to.be.true
-    expect(hasStreamMetadata(action)).to.be.true
-    expect(action.type).to.equal('LOAD_STREAM')
-    expect(action.payload.endpoint).to.be.equal('/test/mock/data/awesome_people.json')
-    expect(action.payload.vo).to.be.empty
-    expect(action.meta.mappingType).to.equal('users')
-    expect(action.meta.renderStream).to.be.a('function')
-  })
+test('#loadAwesomePeople', (assert) => {
+  const action = subject.loadAwesomePeople()
+  assert.true(isFSA(action), 'should contain FSA properties')
+  assert.true(hasStreamMetadata(action), 'should have stream metadata')
+  assert.equal(action.type, 'LOAD_STREAM')
+  assert.equal(action.payload.endpoint, '/test/mock/data/awesome_people.json')
+  assert.deepEqual(action.payload.vo, {})
+  assert.equal(action.meta.mappingType, 'users')
+  assert.equal(typeof action.meta.renderStream, 'function')
+  assert.end()
 })
 
