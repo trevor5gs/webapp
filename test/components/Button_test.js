abc5b8e212633fb5e2b59874e1c9ec68839a38f4
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-08-27 10:49:50 -0600

    Add index file for tests.

---
+++ test/components/Button_test.js
@@ -0,0 +1,13 @@
+import { test } from '../spec_helper'
+// import * as subject from '../../src/components/buttons/Button'
+// import React from 'react/addons'
+
+// const shallowRenderer = React.addons.TestUtils.createRenderer()
+
+test('#render', (assert) => {
+  // const button = shallowRenderer.render(React.createElement(subject, {}, 'My Awesome Button'))
+  // console.log('button ========================', button)
+  assert.true(true)
+  assert.end()
+})
+
---
+++ test/index.js
@@ -0,0 +1,4 @@
+/* eslint no-unused-vars: 0 */
+import * as onboardingActions from './actions/onboarding_actions_test'
+import * as Button from './components/Button_test'
+
