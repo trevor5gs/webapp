9dcbb40266f9abf0b646d90b964a52face91bfb0
Matthew Kitt <mk@mkitt.net>
2015-09-09 02:39:24 -0600

    Test out the alerts
    We should rip this out before we merge into master, it’s just here as a
    demo. We can make use of alerts through uploading the correct file type

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,28 @@ import Navbar from '../components/navigation/Navbar'
 import Modal from '../components/modals/Modal'
 import Devtools from '../components/devtools/Devtools'
 
+// Just testing these
+import { openAlert } from '../actions/modals'
+import AssetErrorDialog from '../components/dialogs/AssetErrorDialog'
+
 class App extends React.Component {
+
+  componentDidMount() {
+    // -------------------------------------
+    // Testing keys also remove from unmounting
+    Mousetrap.bind('g a', () => {
+      return this.props.dispatch(openAlert(<AssetErrorDialog assetType="Avatar" />))
+    })
+    Mousetrap.bind('g c', () => {
+      return this.props.dispatch(openAlert(<AssetErrorDialog assetType="Cover" />))
+    })
+    // -------------------------------------
+  }
+
+  componentWillUnmount() {
+    Mousetrap.unbind('g a')
+    Mousetrap.unbind('g c')
+  }
 
   render() {
     const { location, children } = this.props
@@ -20,10 +41,10 @@ class App extends React.Component {
       </section>
     )
   }
-
 }
 
 App.propTypes = {
+  dispatch: React.PropTypes.func.isRequired,
   location: React.PropTypes.shape({
     pathname: React.PropTypes.string.isRequired,
   }),
