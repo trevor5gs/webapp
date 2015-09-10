0ab888544e050eb43d4629d46e2f1e68766323fa
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-09-10 16:46:11 -0600

    Fix tests.

--- test/actions/onboarding_actions_test.js
+++ test/actions/onboarding_actions_test.js
@@ -8,7 +8,7 @@ describe('onboarding_actions.js', () => {
       expect(isFSA(action)).to.be.true
       expect(hasStreamMetadata(action)).to.be.true
       expect(action.type).to.equal('LOAD_STREAM')
-      expect(action.payload.endpoint).to.be.equal('/data/channels.json')
+      expect(action.payload.endpoint).to.be.equal('https://ello-staging.herokuapp.com/api/v2/interest_categories/members?name=onboarding&per_page=20')
       expect(action.payload.vo).to.be.empty
       expect(action.meta.mappingType).to.equal('users')
       expect(action.meta.renderStream).to.be.a('function')
@@ -21,7 +21,7 @@ describe('onboarding_actions.js', () => {
       expect(isFSA(action)).to.be.true
       expect(hasStreamMetadata(action)).to.be.true
       expect(action.type).to.equal('LOAD_STREAM')
-      expect(action.payload.endpoint).to.be.equal('/data/awesome_people.json')
+      expect(action.payload.endpoint).to.be.equal('https://ello-staging.herokuapp.com/api/v2/interest_categories/members?name=onboarding&per_page=20')
       expect(action.payload.vo).to.be.empty
       expect(action.meta.mappingType).to.equal('users')
       expect(action.meta.renderStream).to.be.a('function')
