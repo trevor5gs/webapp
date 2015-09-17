cfb42396981efbe1ecf776550c4da4319e2a6673
Ryan Tyer <ryan.tyer@gmail.com>
2015-09-16 22:30:20 -0600

    Removed env-dev.js.  Moved dev resources to own folder

--- dev/dev-server.js
+++ dev/dev-server.js
@@ -2,21 +2,26 @@
 var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
-var config = require('./webpack.dev.config')
+var config = require('../webpack.dev.config')
 var app = express()
 var compiler = webpack(config)
 
+// Development Middleware
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }))
-
 app.use(require('webpack-hot-middleware')(compiler))
-app.get('/', function(req, res) {
+
+// Main entry for app
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dev.html'))
 })
+
+// Assets
 app.use(express.static('public/assets'))
 
+// Catchall for any requests like /onboarding
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dev.html'))
 })
