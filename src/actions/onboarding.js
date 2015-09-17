dfeb3f64c8a7065eabc21d1c9f7bf8a792dc62a2
Matthew Kitt <mk@mkitt.net>
2015-09-17 00:46:56 -0600

    Cache communities and people results
    This is so navigating back to these pages provides the same results.

--- src/actions/onboarding.js
+++ src/actions/onboarding.js
@@ -3,20 +3,44 @@ import * as MAPPING_TYPES from '../constants/mapping_types'
 import * as StreamRenderables from '../components/streams/StreamRenderables'
 import * as api from '../api'
 
+let cachedChannel
+let cachedPeople
+
 export function loadChannels() {
-  return {
+  if (cachedChannel) {
+    cachedChannel.payload.endpoint = null
+    return cachedChannel
+  }
+  const cache =  {
     type: ACTION_TYPES.LOAD_STREAM,
     payload: { endpoint: api.channels, vo: {} },
     meta: { mappingType: MAPPING_TYPES.USERS, renderStream: StreamRenderables.onboardingChannels },
   }
+
+  cachedChannel = {
+    ...cache,
+    ...{ type: ACTION_TYPES.LOAD_STREAM_SUCCESS },
+  }
+  return cache
 }
 
+
 export function loadAwesomePeople() {
-  return {
+  if (cachedPeople) {
+    cachedPeople.payload.endpoint = null
+    return cachedPeople
+  }
+  const cache = {
     type: ACTION_TYPES.LOAD_STREAM,
     payload: { endpoint: api.awesomePeoplePath, vo: {} },
     meta: { mappingType: MAPPING_TYPES.USERS, renderStream: StreamRenderables.onboardingPeople },
   }
+
+  cachedPeople = {
+    ...cache,
+    ...{ type: ACTION_TYPES.LOAD_STREAM_SUCCESS },
+  }
+  return cache
 }
 
 export function relationshipBatchSave(ids, priority = 'friend') {
