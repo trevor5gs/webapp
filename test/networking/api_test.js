dbf8bc1d3cafb92504bbd429341d0ec1cb428c66
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-10-20 11:41:37 -0600

    Adds checks for pagingPath being undefined.
    * this errored in the middle of a promise when I switched discover away from using a user, so figured it would be good to add a check for it.

--- test/networking/api_test.js
+++ test/networking/api_test.js
@@ -4,18 +4,22 @@ import * as api from '../../src/networking/api'
 describe('api.js', () => {
   it('#s3CredentialsPath', () => {
     expect(api.s3CredentialsPath().path).to.match(/\/assets\/credentials$/)
+    expect(api.s3CredentialsPath().pagingPath).to.be.undefined
   })
 
   it('#profilePath', () => {
     expect(api.profilePath().path).to.match(/\/profile$/)
+    expect(api.profilePath().pagingPath).to.be.undefined
   })
 
   it('#awesomePeoplePath', () => {
     expect(api.awesomePeoplePath().path).to.match(/\/discover\/users\/onboarding\?/)
+    expect(api.awesomePeoplePath().pagingPath).to.be.undefined
   })
 
   it('#communitiesPath', () => {
     expect(api.communitiesPath().path).to.match(/\/interest_categories\/members\?/)
+    expect(api.communitiesPath().pagingPath).to.be.undefined
   })
 
   it('#relationshipBatchPath', () => {
@@ -25,14 +29,17 @@ describe('api.js', () => {
   it('#discoverRecommended', () => {
     expect(api.discoverRecommended().path).to.match(/\/discover\/users\/recommended\?/)
     expect(api.discoverRecommended().params.include_recent_posts).to.be.true
+    expect(api.discoverRecommended().pagingPath).to.be.undefined
   })
 
   it('#friendStream', () => {
     expect(api.friendStream().path).to.match(/\/streams\/friend\?/)
+    expect(api.friendStream().pagingPath).to.be.undefined
   })
 
   it('#noiseStream', () => {
     expect(api.noiseStream().path).to.match(/\/streams\/noise\?/)
+    expect(api.noiseStream().pagingPath).to.be.undefined
   })
 
   it('#postDetail', () => {
@@ -43,6 +50,7 @@ describe('api.js', () => {
 
   it('#commentsForPost', () => {
     expect(api.commentsForPost({id: 'suck_it'}).path).to.match(/\/posts\/suck_it\/comments\?/)
+    expect(api.commentsForPost({id: 'suck_it'}).pagingPath).to.be.undefined
   })
 
   it('#userDetail', () => {
@@ -52,10 +60,12 @@ describe('api.js', () => {
 
   it('#searchPosts', () => {
     expect(api.searchPosts({ terms: 'blah' }).path).to.match(/\/posts\?terms=blah/)
+    expect(api.searchPosts({ terms: 'blah' }).pagingPath).to.be.undefined
   })
 
   it('#searchUsers', () => {
     expect(api.searchUsers({ terms: 'blah' }).path).to.match(/\/users\?terms=blah/)
+    expect(api.searchUsers({ terms: 'blah' }).pagingPath).to.be.undefined
   })
 })
 
