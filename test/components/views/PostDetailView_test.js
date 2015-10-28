c7a97cd00a6b8a4222c8cb4e98b1c3c6d9f15c89
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-10-28 16:34:42 -0600

    Fix a broken test from a rename.

--- test/components/views/PostDetailView_test.js
+++ test/components/views/PostDetailView_test.js
@@ -4,7 +4,7 @@ import { default as subject } from '../../../src/components/views/PostDetailView
 describe('PostDetailView', () => {
   it('#render', () => {
     const comp = getRenderedComponent(subject, { params: { token: 'yay' } })
-    expect(comp.props.className).to.equal('PostDetail Panel')
+    expect(comp.props.className).to.equal('PostDetailView Panel')
     expect(comp.type).to.equal('section')
     const streamComp = comp.props.children
     expect(streamComp.ref).to.equal('streamComponent')
