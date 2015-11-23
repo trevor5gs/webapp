16bc106202b1d029e6b0ee013ecf371c704e51d8
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-11-23 11:15:23 -0700

    Adds communities and featured users to discover.
    * Hides them from the nav
    * Rename renderable methods to be more generic

--- src/actions/discover.js
+++ src/actions/discover.js
@@ -17,3 +17,27 @@ export function loadDiscoverUsers(type) {
   }
 }
 
+export function loadCommunities() {
+  return {
+    type: ACTION_TYPES.LOAD_STREAM,
+    payload: { endpoint: api.communitiesPath(), vo: {} },
+    meta: {
+      isInitialLoad: true,
+      mappingType: MAPPING_TYPES.USERS,
+      renderStream: StreamRenderables.usersAsCards,
+    },
+  }
+}
+
+export function loadFeaturedUsers() {
+  return {
+    type: ACTION_TYPES.LOAD_STREAM,
+    payload: { endpoint: api.awesomePeoplePath(), vo: {} },
+    meta: {
+      isInitialLoad: true,
+      mappingType: MAPPING_TYPES.USERS,
+      renderStream: StreamRenderables.usersAsGrid,
+    },
+  }
+}
+
