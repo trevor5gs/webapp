518fb95128866cf9a030492c48049ea600693658
Matthew Kitt <mk@mkitt.net>
2015-11-25 15:25:47 -0700

    Adds the banners to logged out search.
    Sort of a bummer here but we broke from the extension. The binding’s 
    get super confused on the calls out to super and throw errors all over
    the place. We may change this down the road, but to keep this moving
    going to just decouple and duplicate the logic for now.

--- src/containers/Find.js
+++ src/containers/Find.js
@@ -1,7 +1,82 @@
-import Search from './Search'
+import React from 'react'
+import { connect } from 'react-redux'
+import classNames from 'classnames'
+import StreamComponent from '../components/streams/StreamComponent'
+import SearchControl from '../components/forms/SearchControl'
+import debounce from 'lodash.debounce'
+import * as SearchActions from '../actions/search'
+import * as ACTION_TYPES from '../constants/action_types'
+import Banderole from '../components/covers/Banderole'
+import { BANDEROLES } from '../constants/gui_types'
 
-class Find extends Search {
+class Find extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = this.props.search
+  }
+
+  componentWillMount() {
+    this.search = debounce(this.search, 300)
+  }
+
+  getAction() {
+    const { terms, type } = this.state
+    if (terms && terms.length > 1) {
+      if (type === 'users') {
+        return SearchActions.searchForUsers(terms)
+      }
+      return SearchActions.searchForPosts(terms)
+    }
+    return null
+  }
+
+  search() {
+    const { dispatch } = this.props
+    dispatch({
+      type: ACTION_TYPES.SEARCH.SAVE,
+      payload: this.state,
+    })
+    const action = this.getAction()
+    if (action) {
+      this.refs.streamComponent.refs.wrappedInstance.setAction(action)
+    }
+  }
+
+  handleControlChange(vo) {
+    this.setState(vo)
+    this.search()
+  }
+
+  render() {
+    const { terms, type } = this.state
+    return (
+      <section className="Search Panel">
+        <Banderole userlist={ BANDEROLES } />
+        <div className="SearchBar">
+          <SearchControl text={terms} controlWasChanged={this.handleControlChange.bind(this)} />
+          <button className={classNames('SearchFilter', { active: type === 'posts' })} onClick={() => { this.handleControlChange({ type: 'posts' }) }} >
+            Posts
+          </button>
+          <button className={classNames('SearchFilter', { active: type === 'users' })} onClick={() => { this.handleControlChange({ type: 'users' }) }} >
+            People
+          </button>
+        </div>
+        <StreamComponent ref="streamComponent" action={this.getAction()} />
+      </section>
+    )
+  }
 }
 
-export default Find
+function mapStateToProps(state) {
+  return {
+    search: state.search,
+  }
+}
+
+Find.propTypes = {
+  dispatch: React.PropTypes.func.isRequired,
+  search: React.PropTypes.object.isRequired,
+}
+
+export default connect(mapStateToProps)(Find)
 
