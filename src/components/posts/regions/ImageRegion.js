b2de6cd68086381bb6e7343efee412d4fe52350b
Matthew Kitt <mk@mkitt.net>
2015-12-01 12:49:38 -0700

    Pull size calculations from the Mothership

--- src/components/posts/regions/ImageRegion.js
+++ src/components/posts/regions/ImageRegion.js
@@ -1,7 +1,50 @@
 import React from 'react'
-import StreamComponent from '../../streams/StreamComponent'
+import { GUI } from '../../../constants/gui_types'
 
 class ImageRegion extends React.Component {
+
+  getAttachmentMetadata() {
+    const { optimized, xhdpi, hdpi } = this.attachment
+    let width = null
+    let height = null
+
+    // Todo: Not sure if we need to calculate hdpi or if xhdpi will work
+    if (optimized && optimized.metadata && optimized.metadata.width) {
+      width = parseInt(optimized.metadata.width, 10)
+      height = parseInt(optimized.metadata.height, 10)
+    } else if (xhdpi && xhdpi.metadata && xhdpi.metadata.width) {
+      width = parseInt(xhdpi.metadata.width, 10)
+      height = parseInt(xhdpi.metadata.height, 10)
+    } else if (hdpi && hdpi.metadata && hdpi.metadata.width) {
+      width = parseInt(hdpi.metadata.width, 10)
+      height = parseInt(hdpi.metadata.height, 10)
+    }
+    return {
+      width: width,
+      height: height,
+      ratio: width ? width / height : null,
+    }
+  }
+
+  // use the lowest of the size constraints to ensure we don't go askew, go
+  // below 1:1 pixel density, or go above the desired grid cell height
+  calculateImageDimensions() {
+    const metadata = this.getAttachmentMetadata()
+    if (!metadata) { return metadata }
+    const { isGridLayout } = this.props
+    const { height, ratio } = metadata
+    const columnWidth = isGridLayout ? GUI.columnWidth : GUI.contentWidth
+    const maxCellHeight = isGridLayout ? 620 : 7500
+    const widthConstrainedRelativeHeight = Math.round(columnWidth * (1 / ratio))
+    const hv = Math.min(widthConstrainedRelativeHeight, height, maxCellHeight)
+    const wv = Math.round(hv * ratio)
+    return {
+      width: wv,
+      height: hv,
+      ratio: ratio,
+    }
+  }
+
 
   isGif() {
     const optimized = this.attachment.optimized
@@ -13,12 +56,13 @@ class ImageRegion extends React.Component {
 
   renderGif() {
     const { content } = this.props
+    const dimensions = this.calculateImageDimensions()
     return (
       <img className="ImageRegion"
         alt={content.alt}
-        height={this.attachment.optimized.metadata.height}
-        src={this.attachment.optimized.url}
-        width={this.attachment.optimized.metadata.width} />
+        width={dimensions.width}
+        height={dimensions.height}
+        src={this.attachment.optimized.url} />
     )
   }
 
@@ -34,19 +78,13 @@ class ImageRegion extends React.Component {
       sizes.push(this.attachment.xhdpi.url + ' 1500w')
       sizes.push(this.attachment.optimized.url + ' 1920w')
     }
-    let height = parseInt(this.attachment.optimized.metadata.height, 10)
-    const width = parseInt(this.attachment.optimized.metadata.width, 10)
-
-    const ratio = width / height
-    height = Math.round(window.columnWidth / ratio)
-
-
     const srcset = sizes.join(', ')
+    const dimensions = this.calculateImageDimensions()
     return (
       <img className="ImageRegion"
         alt={content.alt}
-        height={height}
-        width={window.columnWidth}
+        width={dimensions.width}
+        height={dimensions.height}
         src={this.attachment.hdpi.url}
         srcSet={srcset} />
     )
