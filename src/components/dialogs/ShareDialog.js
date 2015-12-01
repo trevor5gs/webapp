972ffc475227fc31e67c22b0d1a0763a59ce173f
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-12-01 13:53:41 -0700

    Adds the share functionality to the dialog.
    * Adds constants for the share types and dimensions
    * Formats urls correctly for the various services
    * Basically a direct port from the mothership
    * Fixes an issue with the 40px padding on share
    * Passes the author and the post to the dialog
    
    [Finishes #107769798]

--- src/components/dialogs/ShareDialog.js
+++ src/components/dialogs/ShareDialog.js
@@ -10,7 +10,97 @@ import {
   TwitterIcon,
 } from './ShareDialogIcons'
 
+const SHARE_TYPES = {
+  EMAIL: 'email',
+  FACEBOOK: 'facebook',
+  GOOGLE_PLUS: 'google+',
+  LINKEDIN: 'linkedin',
+  PINTEREST: 'pinterest',
+  REDDIT: 'reddit',
+  TUMBLR: 'tumblr',
+  TWITTER: 'twitter',
+}
+
+const SHARE_DIMENSIONS = {}
+SHARE_DIMENSIONS[SHARE_TYPES.FACEBOOK] = { width: 480, height: 210 }
+SHARE_DIMENSIONS[SHARE_TYPES.GOOGLE_PLUS] = { width: 500, height: 385 }
+SHARE_DIMENSIONS[SHARE_TYPES.LINKEDIN] = { width: 550, height: 460 }
+SHARE_DIMENSIONS[SHARE_TYPES.PINTEREST] = { width: 750, height: 320 }
+SHARE_DIMENSIONS[SHARE_TYPES.REDDIT] = { width: 540, height: 420 }
+SHARE_DIMENSIONS[SHARE_TYPES.TUMBLR] = { width: 450, height: 430 }
+SHARE_DIMENSIONS[SHARE_TYPES.TWITTER] = { width: 520, height: 250 }
+
 class ShareDialog extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    const { author, post } = this.props
+    const postLink = `${window.location.protocol}//${window.location.host}/${author.username}/post/${post.token}`
+    let summary = 'Check out this post on Ello'
+    // email string, since we can fit more text content
+    const emailSubject = `${summary}, via @${author.username}`
+    // grab out the image and summary from the post
+    this.image = null
+    for (const region of post.summary) {
+      if (region.kind === 'text') {
+        const div = document.createElement('div')
+        div.innerHTML = region.data
+        summary = div.textContent
+      } else if (!this.image && region.kind === 'image') {
+        this.image = region.data.url
+        if (this.image.indexOf('//') === 0) {
+          this.image = `http:${this.image}`
+        }
+      }
+    }
+    // truncate the tweet summary to be <= 140
+    let tweetSummary = summary
+    if (tweetSummary.length + postLink.length > 139) {
+      tweetSummary = tweetSummary.substr(0, 139 - postLink.length)
+      const summaryArr = tweetSummary.split(' ')
+      summaryArr.pop()
+      tweetSummary = summaryArr.join(' ')
+    }
+    // create "safe" versions of what we need to use
+    this.postLinkSafe = window.encodeURIComponent(postLink)
+    this.summarySafe = window.encodeURIComponent(summary)
+    this.tweetSummarySafe = window.encodeURIComponent(tweetSummary)
+    this.emailSubjectSafe = window.encodeURIComponent(emailSubject)
+    this.emailBodySafe = `${this.summarySafe}%0D%0A%0D%0A${this.postLinkSafe}`
+  }
+
+  getUrl(type) {
+    switch (type) {
+    case SHARE_TYPES.FACEBOOK:
+      return `https://www.facebook.com/sharer/sharer.php?u=${this.postLinkSafe}`
+    case SHARE_TYPES.GOOGLE_PLUS:
+      return `https://plus.google.com/share?url=${this.postLinkSafe}`
+    case SHARE_TYPES.LINKEDIN:
+      return `http://www.linkedin.com/shareArticle?mini=true&url=${this.postLinkSafe}&title=${this.summarySafe}`
+    case SHARE_TYPES.PINTEREST:
+      return `http://pinterest.com/pin/create/button/?url=${this.postLinkSafe}&description=${this.summarySafe}&media=${this.image}`
+    case SHARE_TYPES.REDDIT:
+      return `http://reddit.com/submit?url=${this.postLinkSafe}&title=${this.summarySafe}`
+    case SHARE_TYPES.TUMBLR:
+      return `http://www.tumblr.com/share/link?url=${this.postLinkSafe}&name=${this.summarySafe}`
+    case SHARE_TYPES.TWITTER:
+      return `https://twitter.com/intent/tweet?url=${this.postLinkSafe}&text=${this.tweetSummarySafe}`
+    case SHARE_TYPES.EMAIL:
+    default:
+      return `mailto:?subject=${this.emailSubjectSafe}&body=${this.emailBodySafe}`
+    }
+  }
+
+  popShareWindow(type) {
+    const url = this.getUrl(type)
+    if (url.indexOf('mailto') === 0) {
+      document.location.href = url
+    } else {
+      const width = SHARE_DIMENSIONS[type].width || 700
+      const height = SHARE_DIMENSIONS[type].height || 450
+      window.open(url, 'sharewindow', `width=${width}, height=${height}, left=${window.innerWidth / 2 - width / 2}, top=${window.innerHeight / 2 - height / 2}, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0`)
+    }
+  }
+
   render() {
     return (
       <div className="Dialog ShareDialog">
@@ -19,16 +109,16 @@ class ShareDialog extends React.Component {
           type="url"
           readOnly
           onClick={(e) => e.target.select()}
-          value={this.props.postUrl} />
+          value={this.postLink} />
         <div className="ShareLinks">
-          <button className="ShareLink"><MailIcon/></button>
-          <button className="ShareLink"><FacebookIcon/></button>
-          <button className="ShareLink"><TwitterIcon/></button>
-          <button className="ShareLink"><PinterestIcon/></button>
-          <button className="ShareLink"><GooglePlusIcon/></button>
-          <button className="ShareLink"><TumblrIcon/></button>
-          <button className="ShareLink"><RedditIcon/></button>
-          <button className="ShareLink"><LinkedInIcon/></button>
+          <button className="ShareLink" onClick={() => this.popShareWindow(SHARE_TYPES.EMAIL)}><MailIcon/></button>
+          <button className="ShareLink" onClick={() => this.popShareWindow(SHARE_TYPES.FACEBOOK)}><FacebookIcon/></button>
+          <button className="ShareLink" onClick={() => this.popShareWindow(SHARE_TYPES.TWITTER)}><TwitterIcon/></button>
+          <button className="ShareLink" onClick={() => this.popShareWindow(SHARE_TYPES.PINTEREST)}><PinterestIcon/></button>
+          <button className="ShareLink" onClick={() => this.popShareWindow(SHARE_TYPES.GOOGLE_PLUS)}><GooglePlusIcon/></button>
+          <button className="ShareLink" onClick={() => this.popShareWindow(SHARE_TYPES.TUMBLR)}><TumblrIcon/></button>
+          <button className="ShareLink" onClick={() => this.popShareWindow(SHARE_TYPES.REDDIT)}><RedditIcon/></button>
+          <button className="ShareLink" onClick={() => this.popShareWindow(SHARE_TYPES.LINKEDIN)}><LinkedInIcon/></button>
         </div>
       </div>
     )
@@ -36,7 +126,8 @@ class ShareDialog extends React.Component {
 }
 
 ShareDialog.propTypes = {
-  postUrl: React.PropTypes.string.isRequired,
+  author: React.PropTypes.object.isRequired,
+  post: React.PropTypes.object.isRequired,
 }
 
 export default ShareDialog
