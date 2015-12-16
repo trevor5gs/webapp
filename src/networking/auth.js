04d6a3c36017c6d569bf67641b1f3165623547a0
Jay Zeschin <jay.zeschin@modeset.com>
2015-12-16 13:17:08 -0700

    Revert "Only ask for public privileges"
    This reverts commit 47b95cb58a00364fd3146c10e12ba4b93ed45f60.

--- src/networking/auth.js
+++ src/networking/auth.js
@@ -31,7 +31,7 @@ export function checkAuth(dispatch, oldToken, location) {
   } else {
     const url = ENV.AUTH_DOMAIN + '/api/oauth/authorize.html' +
       '?response_type=token' +
-      '&scope=public' +
+      '&scope=web_app+public' +
       '&client_id=' + ENV.AUTH_CLIENT_ID +
       '&redirect_uri=' + ENV.AUTH_REDIRECT_URI
 
