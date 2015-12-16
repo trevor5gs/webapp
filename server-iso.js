21363d3d78f1ed020ed6d2db353d9bf14d56517c
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2015-12-15 17:07:13 -0700

    Adds ability to get client token from the server.

--- server-iso.js
+++ server-iso.js
@@ -1,3 +1,7 @@
+// load env vars first
+import dotenv from 'dotenv'
+dotenv.load()
+
 import 'newrelic'
 import 'babel-core/polyfill'
 import 'isomorphic-fetch'
@@ -16,7 +20,6 @@ import { updateStrings as updateTimeAgoStrings } from './src/vendor/time_ago_in_
 updateTimeAgoStrings({ about: '' })
 
 const app = express()
-
 let indexStr = ''
 // grab out the index.html string first thing
 fs.readFile(path.join(__dirname, './public/index.html'), 'utf-8', (err, data) => {
@@ -25,10 +28,60 @@ fs.readFile(path.join(__dirname, './public/index.html'), 'utf-8', (err, data) =>
   }
 })
 
-
 // Assets
 app.use(express.static('public'))
 app.use('/static', express.static('public/static'))
+
+// Auth token
+// Get the access token object.
+const credentials = {
+  clientID: process.env.AUTH_CLIENT_ID,
+  clientSecret: process.env.AUTH_CLIENT_SECRET,
+  site: process.env.AUTH_DOMAIN,
+  tokenPath: '/api/oauth/token',
+  headers: {
+    'Accept': 'application/json',
+  },
+}
+
+// Initialize the OAuth2 Library
+const oauth2 = require('simple-oauth2')(credentials)
+const tokenConfig = {}
+let token = null
+
+// Get the access token object for the client
+oauth2.client
+  .getToken(tokenConfig)
+  .then((result) => {
+    if (result.errors) {
+      console.log('Unable to get access token', result)
+      process.exit(1)
+    }
+    token = oauth2.accessToken.create(result)
+  })
+  .catch((error) => {
+    console.log('Access Token error', error.message)
+    process.exit(1)
+  })
+
+app.get('/token', (req, res) => {
+  if (token.expired()) {
+    token.refresh().then((result) => {
+      if (result.errors) {
+        console.log('Access Token error', result)
+        res.status(401).send(result.errors)
+      } else {
+        token = result
+        res.status(200).send(token)
+      }
+    }).catch((error) => {
+      console.log('Access Token error', error.message)
+      res.status(401).send()
+    })
+  } else {
+    res.status(200).send(token)
+  }
+})
 
 // Return promises for initial loads
 function preRender(routerState) {
@@ -54,7 +107,6 @@ function renderFromServer(req, res) {
       const componentHTML = renderToString(InitialComponent)
       const state = store.getState()
       state.authentication.isLoggedIn = false
-      console.log('AUTHENTICATION', state.authentication)
       const initialStateTag = `<script id="initial-state">window.__INITIAL_STATE__ = ${JSON.stringify(state)}</script>`
       indexStr = indexStr.replace('<div id="root"></div>', `<div id="root">${componentHTML}</div>${initialStateTag}`)
       res.send(indexStr)
