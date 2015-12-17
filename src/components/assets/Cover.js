402545c669a66bad90d7bec06213434b79038aa8
Matthew Kitt <mk@mkitt.net>
2015-12-17 07:14:33 -0700

    Preload and fade up the cover images

--- src/components/assets/Cover.js
+++ src/components/assets/Cover.js
@@ -3,6 +3,13 @@ import classNames from 'classnames'
 import { addScrollObject, removeScrollObject } from '../interface/ScrollComponent'
 import { addResizeObject, removeResizeObject } from '../interface/ResizeComponent'
 
+const STATUS = {
+  PENDING: 'isPending',
+  REQUEST: 'isRequesting',
+  SUCCESS: null,
+  FAILURE: 'isFailing',
+}
+
 class Cover extends Component {
   static propTypes = {
     coverImage: PropTypes.any,
@@ -16,17 +23,45 @@ class Cover extends Component {
 
   constructor(props, context) {
     super(props, context)
-    this.state = { asHidden: false }
+    this.state = {
+      asHidden: false,
+      imageSize: 'hdpi',
+      status: STATUS.PENDING,
+    }
+  }
+
+  componentWillMount() {
+    this.setState({ status: STATUS.REQUEST })
   }
 
   componentDidMount() {
     addResizeObject(this)
     addScrollObject(this)
+    if (this.state.status === STATUS.REQUEST) {
+      this.createLoader()
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const thisSource = this.getCoverSource()
+    const nextSource = this.getCoverSource(nextProps)
+    if (thisSource !== nextSource) {
+      this.setState({
+        status: nextSource ? STATUS.REQUEST : STATUS.PENDING,
+      })
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.status === STATUS.REQUEST && !this.img) {
+      this.createLoader()
+    }
   }
 
   componentWillUnmount() {
     removeResizeObject(this)
     removeScrollObject(this)
+    this.disposeLoader()
   }
 
   onResize(resizeProperties) {
@@ -44,28 +79,67 @@ class Cover extends Component {
     }
   }
 
-  getImageSource(coverImage) {
+  getClassNames() {
+    const { isModifiable, modifiers } = this.props
+    const { status, asHidden } = this.state
+    return classNames(
+      'Cover',
+      status,
+      modifiers,
+      { asHidden: asHidden },
+      { isModifiable: isModifiable },
+    )
+  }
+
+  getCoverSource(props = this.props) {
+    const { coverImage } = props
     if (!coverImage) {
       return ''
     } else if (typeof coverImage === 'string') {
       return coverImage
     }
     const { imageSize } = this.state
-    return coverImage[imageSize] ? coverImage[imageSize].url : coverImage.optimized.url
+    return coverImage[imageSize] ? coverImage[imageSize].url : null
+  }
+
+  createLoader() {
+    const src = this.getCoverSource()
+    this.disposeLoader()
+    if (src) {
+      this.img = new Image()
+      this.img.onload = ::this.loadDidSucceed
+      this.img.onerror = ::this.loadDidFail
+      this.img.src = src
+    }
+  }
+
+  disposeLoader() {
+    if (this.img) {
+      this.img.onload = null
+      this.img.onerror = null
+      this.img = null
+    }
+  }
+
+  loadDidSucceed() {
+    this.disposeLoader()
+    this.setState({ status: STATUS.SUCCESS })
+  }
+
+  loadDidFail() {
+    this.disposeLoader()
+    this.setState({ status: STATUS.FAILURE })
   }
 
   render() {
-    const { coverImage, isModifiable, modifiers } = this.props
-    const klassNames = classNames(
-      'Cover',
-      modifiers,
-      { asHidden: this.state.asHidden },
-      { isModifiable: isModifiable },
+    const src = this.getCoverSource()
+    const klassNames = this.getClassNames()
+    const style = src ? { backgroundImage: `url(${src})` } : null
+    return (
+      <div className={klassNames}>
+        <figure className="CoverImage" style={style} />
+      </div>
     )
-    const style = coverImage
-      ? { backgroundImage: `url(${this.getImageSource(coverImage)})` }
-      : null
-    return <div className={klassNames} style={style} />
   }
 }
 
