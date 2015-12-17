6b46c32ffd5133ab405a2f168bb568644eb9b992
Matthew Kitt <mk@mkitt.net>
2015-12-17 04:10:29 -0700

    Build in Avatar loading states
    States are the typical: request, pending, success, 
    failure
    
    Also turned the throbber into a CSS only transition

--- src/components/assets/Avatar.js
+++ src/components/assets/Avatar.js
@@ -2,6 +2,13 @@ import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
 import classNames from 'classnames'
 
+const STATUS = {
+  PENDING: 'isPending',
+  REQUEST: 'isRequesting',
+  SUCCESS: null,
+  FAILURE: 'isFailing',
+}
+
 class Avatar extends Component {
   static propTypes = {
     isModifiable: PropTypes.bool,
@@ -15,36 +22,100 @@ class Avatar extends Component {
     size: 'regular',
   }
 
-  getAvatarSource() {
-    const { sources, size } = this.props
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      status: this.getAvatarSource() ? STATUS.REQUEST : STATUS.PENDING,
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.status === STATUS.REQUEST) {
+      this.createLoader()
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const thisSource = this.getAvatarSource()
+    const nextSource = this.getAvatarSource(nextProps)
+    if (thisSource !== nextSource) {
+      this.setState({
+        status: nextSource ? STATUS.REQUEST : STATUS.PENDING,
+      })
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.status === STATUS.REQUEST && !this.img) {
+      this.createLoader()
+    }
+  }
+
+  componentWillUnmount() {
+    this.disposeLoader()
+  }
+
+  getClassNames() {
+    const { isModifiable } = this.props
+    const { status } = this.state
+    return classNames(
+      'Avatar',
+      status,
+      { isModifiable: isModifiable },
+    )
+  }
+
+  getAvatarSource(props = this.props) {
+    const { sources, size } = props
     if (!sources) {
       return ''
     } else if (typeof sources === 'string') {
       return sources
     }
-    return sources[size] ? sources[size].url : ''
+    return sources[size] ? sources[size].url : null
+  }
+
+  createLoader() {
+    const src = this.getAvatarSource()
+    this.disposeLoader()
+    if (src) {
+      this.img = new Image()
+      this.img.onload = ::this.loadDidSucceed
+      this.img.onerror = ::this.loadDidFail
+      this.img.src = src
+    }
+  }
+
+  disposeLoader() {
+    if (this.img) {
+      this.img.onload = null
+      this.img.onerror = null
+      this.img = null
+    }
+  }
+
+  loadDidSucceed() {
+    this.disposeLoader()
+    this.setState({ status: STATUS.SUCCESS })
+  }
+
+  loadDidFail() {
+    this.disposeLoader()
+    this.setState({ status: STATUS.FAILURE })
   }
 
   render() {
-    const { to, isModifiable } = this.props
-    const style = { backgroundImage: `url(${this.getAvatarSource()})` }
-    // const style = null
-    const klassNames = classNames(
-      'Avatar',
-      { isModifiable: isModifiable },
-    )
-    if (to) {
-      return (
-        <Link to={to} className={klassNames}>
-          <figure className="AvatarImage" style={style}></figure>
-        </Link>
-      )
-    }
-    return (
+    const { to } = this.props
+    const src = this.getAvatarSource()
+    const klassNames = this.getClassNames()
+
+    return to ?
+      <Link to={to} className={klassNames}>
+        <img className="AvatarImage" src={src} />
+      </Link> :
       <span className={klassNames}>
-        <figure className="AvatarImage" style={style}></figure>
+        <img className="AvatarImage" src={src} />
       </span>
-    )
   }
 }
 
