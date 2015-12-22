dc5406fe7e19d42967001b27a0b9b2e5802602f1
Matthew Kitt <mk@mkitt.net>
2015-12-22 09:51:00 -0700

    Add loader states to ImageRegions and fade up
    I didn’t add the loading animation here as I’m not sure if it’s needed
    or not? It’d be just adding the styles/animations at this point as the
    states are already getting set. From a performance standpoint it’d be 
    better not to have one though.

--- src/components/posts/regions/ImageRegion.js
+++ src/components/posts/regions/ImageRegion.js
@@ -1,6 +1,14 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
+import classNames from 'classnames'
 import { GUI } from '../../../constants/gui_types'
+
+const STATUS = {
+  PENDING: 'isPending',
+  REQUEST: 'isRequesting',
+  SUCCESS: null,
+  FAILURE: 'isFailing',
+}
 
 class ImageRegion extends Component {
   static propTypes = {
@@ -9,6 +17,33 @@ class ImageRegion extends Component {
     isGridLayout: PropTypes.bool.isRequired,
     links: PropTypes.object,
     postDetailPath: PropTypes.string,
+  }
+
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      status: STATUS.PENDING,
+    }
+  }
+
+  componentWillMount() {
+    this.setState({ status: STATUS.REQUEST })
+  }
+
+  componentDidMount() {
+    if (this.state.status === STATUS.REQUEST) {
+      this.createLoader()
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.status === STATUS.REQUEST && !this.img) {
+      this.createLoader()
+    }
+  }
+
+  componentWillUnmount() {
+    this.disposeLoader()
   }
 
   getAttachmentMetadata() {
@@ -68,6 +103,35 @@ class ImageRegion extends Component {
     return images.join(', ')
   }
 
+  createLoader() {
+    const srcset = this.getImageSourceSet()
+    this.disposeLoader()
+    if (srcset) {
+      this.img = new Image()
+      this.img.onload = ::this.loadDidSucceed
+      this.img.onerror = ::this.loadDidFail
+      this.img.srcset = srcset
+    }
+  }
+
+  disposeLoader() {
+    if (this.img) {
+      this.img.onload = null
+      this.img.onerror = null
+      this.img = null
+    }
+  }
+
+  loadDidSucceed() {
+    this.disposeLoader()
+    this.setState({ status: STATUS.SUCCESS })
+  }
+
+  loadDidFail() {
+    this.disposeLoader()
+    this.setState({ status: STATUS.FAILURE })
+  }
+
   isGif() {
     const optimized = this.attachment.optimized
     if (optimized && optimized.metadata) {
@@ -123,16 +187,18 @@ class ImageRegion extends Component {
 
   renderAttachmentAsLink() {
     const { postDetailPath } = this.props
+    const { status } = this.state
     return (
-      <Link to={postDetailPath} className="ImageRegion">
+      <Link to={postDetailPath} className={classNames('ImageRegion', status)}>
         {this.renderAttachment()}
       </Link>
     )
   }
 
   renderAttachmentAsStatic() {
+    const { status } = this.state
     return (
-      <div className="ImageRegion">
+      <div className={classNames('ImageRegion', status)}>
         {this.renderAttachment()}
       </div>
     )
