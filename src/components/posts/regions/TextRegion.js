7ddd238469b4962221cae785c9695a6d16133be0
Matthew Kitt <mk@mkitt.net>
2015-12-22 07:17:36 -0700

    Connect TextRegions to handle relative links
    This allows links that are set as relative: usernames, markdown links, 
    raw links and eventually hashtags to be sent through the router. We
    still have the special hashtag case in here but that can eventually get
    removed once we change it in the API.
    
    Eventually we’ll need to deal with rerouting all hashtag links through
    `/find` but that can wait till we release the web app to logged in users.
    
    Wired up the linking logic on reposted items as well.

--- src/components/posts/regions/TextRegion.js
+++ src/components/posts/regions/TextRegion.js
@@ -1,28 +1,40 @@
 import React, { Component, PropTypes } from 'react'
-import ReactDOM from 'react-dom'
-import { Link } from 'react-router'
+import { connect } from 'react-redux'
+import { pushState } from 'redux-router'
 import classNames from 'classnames'
 
 class TextRegion extends Component {
   static propTypes = {
     content: PropTypes.string.isRequired,
+    dispatch: PropTypes.func.isRequired,
     isGridLayout: PropTypes.bool.isRequired,
     postDetailPath: PropTypes.string,
   }
 
-  handleGridRegionClick(e) {
-    const { MoreButton } = this.refs
-    if (e.target.classList.contains('hashtag-link')) {
+  handleRegionClick(e) {
+    const { dispatch, isGridLayout, postDetailPath } = this.props
+    const { classList, dataset, nodeName } = e.target
+    // Get the raw value instead of the property value which is always absolute
+    const href = e.target.getAttribute('href')
+
+    // Relative links get sent to pushState (usernames, raw links, hashtags)
+    if (href && href[0] === '/') {
       e.preventDefault()
-      const href = e.target.getAttribute('data-href').replace(/^\/search/, '/find')
-      const link = document.createElement('a')
-      link.href = href
-      return link.click()
-    }
-    if (e.target.nodeName !== 'A') {
+      return dispatch(pushState(window.history.state, href))
+
+    // We have a special `span` based fake link at the moment we have to test
+    // for. Once we change this back to an `<a> element we can rip this out.
+    } else if (classList.contains('hashtag-link')) {
       e.preventDefault()
-      return ReactDOM.findDOMNode(MoreButton).click()
+      return dispatch(pushState(window.history.state, dataset.href.replace(/^\/search/, '/find')))
+
+    // Treat non links within grid layouts as a push to it's detail path
+    } else if (isGridLayout && postDetailPath && nodeName !== 'A') {
+      e.preventDefault()
+      return dispatch(pushState(window.history.state, postDetailPath))
     }
+    // The alternative is it's either in list and we ignore it or it's an
+    // absolute link and we allow it's default behavior.
   }
 
   render() {
@@ -33,18 +45,12 @@ class TextRegion extends Component {
         <div
           className={classNames('RegionContent', { asHotRegion: isHotRegion })}
           dangerouslySetInnerHTML={{ __html: content }}
-          onClick={isHotRegion ? (::this.handleGridRegionClick) : null}
+          onClick={::this.handleRegionClick}
           />
-          <Link
-            className="invisible"
-            ref="MoreButton"
-            to={postDetailPath}>
-            More
-          </Link>
       </div>
     )
   }
 }
 
-export default TextRegion
+export default connect()(TextRegion)
 
