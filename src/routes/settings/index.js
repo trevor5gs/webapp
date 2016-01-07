f593516c335e5f07de7d2ee6a4cdd9ac17ca0bc9
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-01-07 16:05:52 -0700

    Update the settings route.

--- src/routes/settings/index.js
+++ src/routes/settings/index.js
@@ -3,7 +3,7 @@ export default [
     path: 'settings',
     getComponents(location, cb) {
       // require.ensure([], (require) => {
-      cb(null, require('../../containers/settings/settings').default)
+      cb(null, require('../../containers/settings/Settings').default)
       // })
     },
   },
