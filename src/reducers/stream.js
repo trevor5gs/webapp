04aa902899a66dc0664d52095b94a92d999a80d0
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-01-08 12:08:10 -0700

    Don’t add errors from posts to the stream reducer.

--- src/reducers/stream.js
+++ src/reducers/stream.js
@@ -1,7 +1,7 @@
 export function stream(state = {}, action = { type: '' }) {
   if (action.type.indexOf('LOAD_STREAM') === 0 ||
       action.type.indexOf('LOAD_NEXT_CONTENT') === 0 ||
-      action.type.indexOf('POST.') === 0) {
+      (action.type.indexOf('POST.') === 0 && action.type.indexOf('SUCCESS') > -1)) {
     return {
       ...state,
       error: action.error,
