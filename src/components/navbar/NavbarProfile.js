9e4121198f69b0cfabe82c8ddf834b09ad66b618
Matthew Kitt <mk@mkitt.net>
2016-01-08 14:51:11 -0700

    Adds the NavbarProfileLinks for logged in users
    - [x] Adds major and minor links
    - [x] Experimenting with the nav appearing on hover instead of click
    - [x] Change the Community link to pull up our community picker instead 
    of redirecting to WTF
    
    Current links are:
    - @username
    - Loves
    - Invite
    - Settings
    - Communities (/discover/communities)
    - Help
    - Get the app
    - Store
    - Logout

--- src/components/navbar/NavbarProfile.js
+++ src/components/navbar/NavbarProfile.js
@@ -1,14 +1,30 @@
 import React, { Component, PropTypes } from 'react'
 import Avatar from '../assets/Avatar'
+import { Link } from 'react-router'
+
+const appleStoreLink = 'http://appstore.com/ello/ello'
+const threadlessLink = 'http://ello.threadless.com/'
 
 class NavbarProfile extends Component {
-
   render() {
     const { avatar, username } = this.props
     if (avatar && username) {
       return (
         <span className="NavbarProfile">
           <Avatar to={`/${username}`} sources={avatar} />
+          <nav className="NavbarProfileLinks">
+            <Link className="NavbarProfileLink" to={`/${username}`}>{`@${username}`}</Link>
+            <Link className="NavbarProfileLink" to={`/${username}/loves`}>Loves</Link>
+            <Link className="NavbarProfileLink" to="/invitations">Invite</Link>
+            <Link className="NavbarProfileLink" to="/settings">Settings</Link>
+            <hr className="NavbarProfileLinkDivider"/>
+            <Link className="NavbarProfileLink" to="/discover/communities">Communities</Link>
+            <a className="NavbarProfileLink" href="/wtf" target="_blank">Help</a>
+            <a className="NavbarProfileLink" href={appleStoreLink} target="_blank">Get the app</a>
+            <a className="NavbarProfileLink" href={threadlessLink} target="_blank">Store</a>
+            <a className="NavbarProfileLink" href="/logout" rel="nofollow">Logout</a>
+          </nav>
+
         </span>
       )
     }
