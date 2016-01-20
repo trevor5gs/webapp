67c519e321592454787f5f54b3952e55eb8374a7
Matthew Kitt <mk@mkitt.net>
2016-01-20 06:29:21 -0700

    Remove the NewSessionForm and handle in container

--- src/containers/authentication/SignIn.js
+++ src/containers/authentication/SignIn.js
@@ -3,10 +3,19 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import random from 'lodash.random'
 import { AUTHENTICATION_PROMOTIONS } from '../../constants/promotion_types'
+import { FORM_CONTROL_STATUS as STATUS } from '../../constants/gui_types'
+import { getUserCredentials } from '../../actions/authentication'
 import { trackEvent } from '../../actions/tracking'
 import Cover from '../../components/assets/Cover'
 import Credits from '../../components/assets/Credits'
-import NewSessionForm from '../../components/forms/NewSessionForm'
+import EmailControl from '../../components/forms/EmailControl'
+import PasswordControl from '../../components/forms/PasswordControl'
+import FormButton from '../../components/forms/FormButton'
+import {
+  isFormValid,
+  getEmailStateFromClient,
+  getPasswordState,
+} from '../../components/forms/Validators'
 import AppleStoreLink from '../../components/support/AppleStoreLink'
 
 class SignIn extends Component {
@@ -14,8 +23,16 @@ class SignIn extends Component {
   constructor(props, context) {
     super(props, context)
     this.state = {
+      emailState: { status: STATUS.INDETERMINATE, message: '' },
+      passwordState: { status: STATUS.INDETERMINATE, message: '' },
       featuredUser: null,
     }
+    this.emailValue = ''
+    this.passwordValue = ''
+    this.handleSubmit = ::this.handleSubmit
+    this.creditsTrackingEvent = ::this.creditsTrackingEvent
+    this.emailControlWasChanged = ::this.emailControlWasChanged
+    this.passwordControlWasChanged = ::this.passwordControlWasChanged
   }
 
   componentWillMount() {
@@ -29,21 +46,69 @@ class SignIn extends Component {
     dispatch(trackEvent('authentication-credits-clicked'))
   }
 
+  // TODO: Need to handle the return error or success when this is submitted
+  handleSubmit(e) {
+    e.preventDefault()
+    const { dispatch } = this.props
+    dispatch(getUserCredentials(this.emailValue, this.passwordValue))
+  }
+
+  emailControlWasChanged({ email }) {
+    this.emailValue = email
+    const { emailState } = this.state
+    const currentStatus = emailState.status
+    const newState = getEmailStateFromClient({ value: email, currentStatus })
+    if (newState.status !== currentStatus) {
+      this.setState({ emailState: newState })
+    }
+  }
+
+  passwordControlWasChanged({ password }) {
+    this.passwordValue = password
+    const { passwordState } = this.state
+    const currentStatus = passwordState.status
+    const newState = getPasswordState({ value: password, currentStatus })
+    if (newState.status !== currentStatus) {
+      this.setState({ passwordState: newState })
+    }
+  }
+
   render() {
-    const { featuredUser } = this.state
+    const { emailState, passwordState, featuredUser } = this.state
+    const isValid = isFormValid([emailState, passwordState])
     return (
       <section className="Authentication Panel">
         <div className="FormDialog">
           <h1>
-            <img src="/static/images/support/v.png" width="32" height="32" alt="hi" />
+            <img src="/static/images/support/v.png" width="32" height="32" alt="Hi" />
             Welcome back.
           </h1>
-          <NewSessionForm/>
+          <form
+            className="AuthenticationForm"
+            id="NewSessionForm"
+            noValidate="novalidate"
+            onSubmit={ this.handleSubmit }
+            role="form"
+          >
+            <EmailControl
+              classList="asBoxControl"
+              label={ `Email ${emailState.message}` }
+              onChange={ this.emailControlWasChanged }
+              tabIndex="1"
+            />
+            <PasswordControl
+              classList="asBoxControl"
+              label={ `Password ${passwordState.message}` }
+              onChange={ this.passwordControlWasChanged }
+              tabIndex="2"
+            />
+            <FormButton disabled={ !isValid } tabIndex="3">Enter Ello</FormButton>
+          </form>
           <Link className="ForgotPasswordLink" to="/forgot-password">Forgot password?</Link>
         </div>
         <AppleStoreLink/>
-        <Credits clickAction={::this.creditsTrackingEvent} user={featuredUser} />
-        <Cover coverImage={featuredUser.coverImage} modifiers="asFullScreen withOverlay" />
+        <Credits clickAction={ this.creditsTrackingEvent } user={ featuredUser } />
+        <Cover coverImage={ featuredUser.coverImage } modifiers="asFullScreen withOverlay" />
       </section>
     )
   }
