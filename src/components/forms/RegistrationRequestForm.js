fde61b4650f7ceab9acfd91d59a7598366a11516
Matthew Kitt <mk@mkitt.net>
2016-01-21 04:32:03 -0700

    Cleanup RegistrationRequestForm

--- src/components/forms/RegistrationRequestForm.js
+++ src/components/forms/RegistrationRequestForm.js
@@ -1,100 +1,79 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import debounce from 'lodash.debounce'
 import { FORM_CONTROL_STATUS as STATUS } from '../../constants/gui_types'
-import { requestInvite/* , checkAvailability */ } from '../../actions/profile'
+import { requestInvite } from '../../actions/profile'
 import FormButton from '../forms/FormButton'
 import EmailControl from '../forms/EmailControl'
+import { isFormValid, getEmailStateFromClient } from '../forms/Validators'
 
 class RegistrationRequestForm extends Component {
 
   constructor(props, context) {
     super(props, context)
     this.state = {
-      emailStatus: STATUS.INDETERMINATE,
-      emailSuggestion: null,
+      formStatus: STATUS.INDETERMINATE,
+      emailState: { status: STATUS.INDETERMINATE, message: '' },
     }
-  }
-
-  componentWillMount() {
-    this.handleEmailControlChanged = debounce(this.handleEmailControlChanged, 500)
+    this.handleSubmit = ::this.handleSubmit
+    this.emailControlWasChanged = ::this.emailControlWasChanged
   }
 
   componentWillReceiveProps(nextProps) {
+    this.emailValue = ''
     const { availability } = nextProps
     if (availability && availability.hasOwnProperty('email')) {
       this.onValidateEmailResponse(availability)
     }
   }
 
-  onValidateEmailResponse(availability) {
-    const { emailStatus } = this.state
-    if (!availability && emailStatus !== STATUS.FAILURE) {
-      return this.setState({ emailStatus: STATUS.FAILURE, emailSuggestion: null })
-    }
-    const { email, suggestions } = availability
-    const emailFullSuggestion = suggestions.email &&
-      suggestions.email.full &&
-      suggestions.email.full.length ?
-      suggestions.email.full :
-      null
-    if (email && emailStatus !== STATUS.SUCCESS) {
-      return this.setState({ emailStatus: STATUS.SUCCESS, emailSuggestion: emailFullSuggestion })
-    } else if (!email && emailStatus !== STATUS.FAILURE) {
-      return this.setState({ emailStatus: STATUS.FAILURE, emailSuggestion: emailFullSuggestion })
-    }
-  }
-
-  handleEmailControlChanged(vo) {
-    const { emailStatus } = this.state
-    if (!vo.email.length && emailStatus !== STATUS.INDETERMINATE) {
-      return this.setState({ emailStatus: STATUS.INDETERMINATE, emailSuggestion: null })
-    }
-    if (emailStatus !== STATUS.REQUEST) {
-      this.setState({ emailStatus: STATUS.REQUEST, emailSuggestion: null })
-    }
-    // this.props.dispatch(checkAvailability(vo))
-    // TODO: this is temporary until availability endpoints are public
-    if (vo.email.match(/(.+)@(.+)\.([a-z]{2,})/)) {
-      this.setState({ emailStatus: STATUS.SUCCESS, emailSuggestion: null })
-    } else {
-      this.setState({ emailStatus: STATUS.FAILURE, emailSuggestion: null })
-    }
-  }
-
   handleSubmit(e) {
     e.preventDefault()
-    const { emailStatus } = this.state
-    if (emailStatus === STATUS.SUCCESS) {
-      this.props.dispatch(requestInvite(this.refs.emailControl.refs.input.value))
-      this.setState({ emailStatus: STATUS.SUBMITTED, emailSuggestion: null })
+    const { dispatch } = this.props
+    dispatch(requestInvite(this.emailValue))
+    this.setState({ formStatus: STATUS.SUBMITTED })
+  }
+
+  emailControlWasChanged({ email }) {
+    this.emailValue = email
+    const { emailState } = this.state
+    const currentStatus = emailState.status
+    const newState = getEmailStateFromClient({ value: email, currentStatus })
+    if (newState.status !== currentStatus) {
+      this.setState({ emailState: newState })
     }
+  }
+
+  renderSubmitted() {
+    return (
+      <div>Please check your email to join Ello</div>
+    )
+  }
+
+  renderForm() {
+    const { emailState } = this.state
+    const isValid = isFormValid([emailState])
+    return (
+      <form
+        className="AuthenticationForm"
+        id="RegistrationRequestForm"
+        noValidate="novalidate"
+        onSubmit={ this.handleSubmit }
+        role="form"
+      >
+        <EmailControl
+          classList="asBoxControl"
+          label={ `Email ${emailState.message}` }
+          onChange={ this.emailControlWasChanged }
+          tabIndex="1"
+        />
+        <FormButton disabled={ !isValid } tabIndex="2">Sign up</FormButton>
+      </form>
+    )
   }
 
   render() {
-    const { emailStatus, emailSuggestion } = this.state
-    const isFormValid = emailStatus === STATUS.SUCCESS
-    const wasSubmitted = emailStatus === STATUS.SUBMITTED
-    return wasSubmitted ?
-      <div>Please check your email to join Ello.</div> :
-      <form
-        id="RegistrationRequestForm"
-        className="AuthenticationForm"
-        onSubmit={::this.handleSubmit}
-        role="form"
-        noValidate="novalidate"
-      >
-        <EmailControl
-          ref="emailControl"
-          tabIndex="1"
-          text=""
-          status={emailStatus}
-          suggestions={emailSuggestion}
-          controlWasChanged={::this.handleEmailControlChanged}
-          classModifiers="asBoxControl"
-        />
-        <FormButton tabIndex="2" disabled={!isFormValid}>Sign up</FormButton>
-      </form>
+    const { formStatus } = this.state
+    return formStatus === STATUS.SUBMITTED ? this.renderSubmitted() : this.renderForm()
   }
 }
 
@@ -102,11 +81,5 @@ RegistrationRequestForm.propTypes = {
   dispatch: PropTypes.func.isRequired,
 }
 
-function mapStateToProps(state) {
-  return {
-    availability: state.profile.availability,
-  }
-}
-
-export default connect(mapStateToProps)(RegistrationRequestForm)
+export default connect()(RegistrationRequestForm)
 
