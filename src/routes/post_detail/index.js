e175368e54ba5a82cb58aada17b2065d09281bd0
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-02-02 13:29:30 -0700

    Add back in the redirects to the mothership.

--- src/routes/post_detail/index.js
+++ src/routes/post_detail/index.js
@@ -3,5 +3,10 @@ export default {
   getComponents(location, cb) {
     cb(null, require('../../containers/details/PostDetail').default)
   },
+  onEnter(nextState, replaceState, callback) {
+    if (callback) {
+      document.location.href = ENV.REDIRECT_URI + nextState.location.pathname
+    }
+  },
 }
 
--- src/routes/user_detail/index.js
+++ src/routes/user_detail/index.js
@@ -1,20 +1,27 @@
-const TYPES = [
-  'following',
-  'followers',
-  'loves',
-]
+// TODO: uncomment this to lock down user paths
+// const TYPES = [
+//   'following',
+//   'followers',
+//   'loves',
+// ]
 
 export default {
   path: ':username(/:type)',
   getComponents(location, cb) {
     cb(null, require('../../containers/details/UserDetail').default)
   },
-  onEnter(nextState, replaceState) {
-    const type = nextState.params.type
-    // redirect back to /username if type is unrecognized
-    if (type && TYPES.indexOf(type) === -1) {
-      replaceState(nextState, `/${nextState.params.username}`)
+  onEnter(nextState, replaceState, callback) {
+    if (callback) {
+      document.location.href = ENV.REDIRECT_URI + nextState.location.pathname
     }
   },
+  // TODO: uncomment this to lock down user paths
+  // onEnter(nextState, replaceState) {
+  //   const type = nextState.params.type
+  //   // redirect back to /username if type is unrecognized
+  //   if (type && TYPES.indexOf(type) === -1) {
+  //     replaceState(nextState, `/${nextState.params.username}`)
+  //   }
+  // },
 }
 
