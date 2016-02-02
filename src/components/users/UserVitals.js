b5997bdbb038bb187e6471d489eafa15ae1cc700
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-02-02 13:45:29 -0700

    Add logged out version of user stats.

--- src/components/users/UserVitals.js
+++ src/components/users/UserVitals.js
@@ -58,6 +58,49 @@ UserStats.propTypes = {
   user: PropTypes.shape({}).isRequired,
 }
 
+export const LoggedOutUserStats = ({ user }) =>
+  <div className="UserStats">
+    <dl>
+      <span className="UserStatsLink asDisabled" activeClassName="active" to={`/${user.username}`}>
+        <dt>{user.postsCount}</dt>
+        <dd>Posts</dd>
+      </span>
+    </dl>
+    <dl>
+      <span
+        className="UserStatsLink asDisabled"
+        activeClassName="active"
+        to={`/${user.username}/following`}
+      >
+        <dt>{user.followingCount}</dt>
+        <dd>Following</dd>
+      </span>
+    </dl>
+    <dl>
+      <span
+        className="UserStatsLink asDisabled"
+        activeClassName="active"
+        to={`/${user.username}/followers`}
+      >
+        <dt>{user.followersCount}</dt>
+        <dd>Followers</dd>
+      </span>
+    </dl>
+    <dl>
+      <span
+        className="UserStatsLink asDisabled"
+        activeClassName="active"
+        to={`/${user.username}/loves`}
+      >
+        <dt>{user.lovesCount}</dt>
+        <dd>Loves</dd>
+      </span>
+    </dl>
+  </div>
+
+LoggedOutUserStats.propTypes = {
+  user: PropTypes.shape({}).isRequired,
+}
 
 export const UserInfo = ({ user }) => {
   let externalLinks = []
