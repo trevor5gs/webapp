dda00a3d46a1e66ac164e1bc80749355961626cb
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-02-09 16:54:42 -0700

    Remove unused preference constant labels

--- src/constants/gui_types.js
+++ src/constants/gui_types.js
@@ -45,78 +45,6 @@ export const SETTINGS = {
 }
 
 export const PREFERENCES = {
-  PREF_PUBLIC_PROFILE: {
-    term: 'Public Profile',
-    desc: 'Make my profile viewable by people outside of the Ello network.',
-  },
-  PREF_COMMENTS: {
-    term: 'Comments',
-    desc: 'Allow people to comment on my posts.',
-  },
-  PREF_LOVES: {
-    term: 'Loves',
-    desc: 'Allow people to love my posts.',
-  },
-  PREF_SHARING: {
-    term: 'Sharing',
-    desc: 'Allow people to share my posts.',
-  },
-  PREF_REPOSTING: {
-    term: 'Reposting',
-    desc: 'Allow people to repost my posts.',
-  },
-  PREF_EMBEDDED_MEDIA: {
-    term: 'Embedded Media',
-    desc: 'Warn me when media may contain 3rd party ads.',
-  },
-  PREF_ANALYTICS: {
-    term: 'Analytics',
-    desc: 'Allow Ello to gather anonymous information about your visit, which helps us make Ello better. <a href="https://ello.co/wtf/post/privacy" target="_blank">Learn more</a>.',
-  },
-  PREF_DISCOVERABILITY: {
-    term: 'Discoverablility',
-    desc: 'Let friends who have my e-mail address find me on Ello.',
-  },
-  MAIL_COMMENTS: {
-    term: 'Comments',
-    desc: 'Receive an e-mail when other people comment on your posts.',
-  },
-  MAIL_LOVES: {
-    term: 'Loves',
-    desc: 'Receive an e-mail when other people love your posts.',
-  },
-  MAIL_MENTIONS: {
-    term: 'Mentions',
-    desc: 'Receive an e-mail when other people mention you in a post.',
-  },
-  MAIL_REPOSTS: {
-    term: 'Reposts',
-    desc: 'Receive an e-mail when other people repost your posts.',
-  },
-  MAIL_NEW_FOLLOWERS: {
-    term: 'New Followers',
-    desc: 'Receive an e-mail when other people follow you.',
-  },
-  MAIL_INVITE_ACCEPTED: {
-    term: 'Invitations',
-    desc: 'Receive an e-mail when other people accept your invitations.',
-  },
-  NEWS_FEATURES: {
-    term: 'News & Features',
-    desc: 'Receive e-mails from Ello with service updates and feature announcements.',
-  },
-  NEWS_WEEKLY: {
-    term: 'Weekly Ello',
-    desc: 'Receive a weekly curated e-mail of inspiring Ello posts.',
-  },
-  NEWS_DAILY: {
-    term: 'Daily Ello',
-    desc: 'Receive a daily curated e-mail of inspiring Ello posts.',
-  },
-  NEWS_TIPS: {
-    term: 'Tips for Getting Started',
-    desc: 'Receive e-mails with tips and tricks on how to use Ello.',
-  },
   NSFW_VIEW: {
     term: 'View Adult Content',
     desc: '<a href="https://ello.co/wtf/post/rules" target="_blank">What does this mean?</a>',
@@ -126,3 +54,4 @@ export const PREFERENCES = {
     desc: '<a href="https://ello.co/wtf/post/rules" target="_blank">What does this mean?</a>',
   },
 }
+
