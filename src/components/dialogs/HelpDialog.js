a8368ce47333b42499cd7c5220b918b51c076445
Matthew Kitt <mk@mkitt.net>
2016-02-11 08:09:46 -0700

    Default heading tags to sanserif instead of bold

--- src/components/dialogs/HelpDialog.js
+++ src/components/dialogs/HelpDialog.js
@@ -8,12 +8,12 @@ const HelpDialog = () =>
     <p><span className="ShortcutLabel">{ SHORTCUT_KEYS.SEARCH }</span> Navigate to Search</p>
     <p><span className="ShortcutLabel">{ SHORTCUT_KEYS.DISCOVER }</span> Navigate to Discover</p>
     <p><span className="ShortcutLabel">{ SHORTCUT_KEYS.ONBOARDING }</span> Navigate to Onboarding</p>
-    <p><span className="ShortcutLabel" style={{ textIndent: 15 }}>{ SHORTCUT_KEYS.TOGGLE_LAYOUT }</span> Toggle grid mode for main content</p>
+    <p><span className="ShortcutLabel">{ SHORTCUT_KEYS.TOGGLE_LAYOUT }</span> Toggle grid mode for main content</p>
     <p><span className="ShortcutLabel">{ SHORTCUT_KEYS.ESC.toUpperCase() }</span> Close modal or alerts</p>
     <p><span className="ShortcutLabel">{ SHORTCUT_KEYS.FULLSCREEN }</span> Toggle fullscreen post editor</p>
     <p><span className="ShortcutLabel">{ SHORTCUT_KEYS.DT_GRID_TOGGLE }</span> Toggle layout grid</p>
     <p><span className="ShortcutLabel">{ SHORTCUT_KEYS.DT_GRID_CYCLE }</span> Toggle between horizontal and vertical grid</p>
-    <p><span className="ShortcutLabel" style={{ textIndent: 15 }}>{ SHORTCUT_KEYS.HELP }</span> Show this help modal</p>
+    <p><span className="ShortcutLabel">{ SHORTCUT_KEYS.HELP }</span> Show this help modal</p>
   </div>
 
 export default HelpDialog
