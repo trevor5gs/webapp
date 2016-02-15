36adff3241aba2203544f4356e5b6091209bf257
Matthew Kitt <mk@mkitt.net>
2016-02-15 10:41:00 -0700

    Use an icon for the close button on Alerts

--- src/components/dialogs/Dialog.js
+++ src/components/dialogs/Dialog.js
@@ -1,11 +1,20 @@
 import React, { PropTypes } from 'react'
+import { SVGIcon } from '../interface/SVGComponents'
+
+const DismissIcon = () =>
+  <SVGIcon className="DialogDismissIcon">
+    <g>
+      <line x1="6" x2="14" y1="6" y2="14"/>
+      <line x1="14" x2="6" y1="6" y2="14"/>
+    </g>
+  </SVGIcon>
 
 const Dialog = ({ body, title, onClick }) =>
   <div className="Dialog">
     <h2>{title}</h2>
     <p>{body}</p>
     { onClick ?
-      <button className="DialogClose" onClick={ onClick }>x</button> :
+      <button className="DialogClose" onClick={ onClick }><DismissIcon/></button> :
       null
     }
   </div>
