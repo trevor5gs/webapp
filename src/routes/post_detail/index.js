dde90e1189e9af7aa9c3fb35009e93d5974a7486
Michael Westbom <michael@agilion.com>
2016-02-23 14:02:43 -0500

    Drop onenter hooks that bust up detail views

--- src/routes/post_detail/index.js
+++ src/routes/post_detail/index.js
@@ -3,10 +3,10 @@ export default {
   getComponents(location, cb) {
     cb(null, require('../../containers/details/PostDetail').default)
   },
-  onEnter(nextState, replaceState, callback) {
-    if (callback) {
-      document.location.href = ENV.REDIRECT_URI + nextState.location.pathname
-    }
-  },
+  // TODO: kill this whole onEnter before merging to master
+  // onEnter(nextState, replaceState, callback) {
+  //   if (callback) {
+  //     document.location.href = ENV.REDIRECT_URI + nextState.location.pathname
+  //   }
+  // },
 }
-
