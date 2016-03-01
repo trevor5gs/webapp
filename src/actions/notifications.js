d55c4daec8ee50ddd9164ab4c335614e4a002a9b
Matthew Kitt <mk@mkitt.net>
2016-03-01 06:00:30 -0700

    Add a notification zero state

--- src/actions/notifications.js
+++ src/actions/notifications.js
@@ -1,8 +1,10 @@
+import React from 'react'
 import * as ACTION_TYPES from '../constants/action_types'
 import * as MAPPING_TYPES from '../constants/mapping_types'
 import * as api from '../networking/api'
 import * as StreamFilters from '../components/streams/StreamFilters'
 import * as StreamRenderables from '../components/streams/StreamRenderables'
+import { ZeroState } from '../components/zeros/Zeros'
 
 export function loadNotifications(params = {}) {
   return {
@@ -13,6 +15,7 @@ export function loadNotifications(params = {}) {
       renderStream: {
         asList: StreamRenderables.notificationList,
         asGrid: StreamRenderables.notificationList,
+        asZero: <ZeroState>Sorry, no notifications found.</ZeroState>,
       },
       resultFilter: StreamFilters.notificationsFromActivities,
     },
