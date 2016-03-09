3cb5b0ada9ff03f1dc66c1c0b65a44dcf01a9718
Michael Westbom <michael@agilion.com>
2016-03-09 14:38:51 -0500

    Revert remove isomorphic commit
    This reverts commit 70540dbd6e0038eff67ea0a81dd863c95374882f.

--- server-iso.js
+++ server-iso.js
@@ -9,8 +9,15 @@ import throng from 'throng'
 import librato from 'librato-node'
 import path from 'path'
 import fs from 'fs'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { match, RouterContext } from 'react-router'
+import { Provider } from 'react-redux'
+import store from './src/store'
 import { updateStrings as updateTimeAgoStrings } from './src/vendor/time_ago_in_words'
 import addOauthRoute from './oauth'
+import routes from './src/routes'
+import { replace } from 'react-router-redux'
 
 // load env vars first
 require('dotenv').load({ silent: process.env.NODE_ENV === 'production' })
@@ -47,9 +54,63 @@ addOauthRoute(app)
 app.use(express.static('public'))
 app.use('/static', express.static('public/static'))
 
+// Return promises for initial loads
+function preRender(renderProps) {
+  const promises = renderProps.components.map(component => (component && component.preRender) ? component.preRender(store, renderProps) : null).filter(component => !!component)
+  return Promise.all(promises)
+}
+
+function renderFromServer(req, res) {
+  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+    // populate the rouer store object for initial render
+    store.dispatch(replace(renderProps.location.pathname))
+    if (error) {
+      console.log('ELLO MATCH ERROR', error)
+    } else if (redirectLocation) {
+      console.log('ELLO HANDLE REDIRECT', redirectLocation)
+    } else if (!renderProps) { return }
+    preRender(renderProps).then(() => {
+      const InitialComponent = (
+        <Provider store={store}>
+          <RouterContext {...renderProps} />
+        </Provider>
+      )
+      const componentHTML = renderToString(InitialComponent)
+      const state = store.getState()
+      const initialStateTag = `<script id="initial-state">window.__INITIAL_STATE__ = ${JSON.stringify(state)}</script>`
+      indexStr = indexStr.replace('<div id="root"></div>', `<div id="root">${componentHTML}</div>${initialStateTag}`)
+      res.send(indexStr)
+    }).catch((err) => {
+      // this will give you a js error like:
+      // `window is not defined`
+      console.log('ELLO CATCH ERROR', err)
+    })
+  })
+}
+
+const loggedOutPaths = {
+  explore: /^\/explore/,
+  explore_recent: /^\/explore\/recent/,
+  explore_trending: /^\/explore\/trending/,
+  find: /^\/find$/,
+  forgot_password: /^\/forgot-password/,
+  signup: /^\/signup/,
+}
+
 app.use((req, res) => {
-  console.log('ELLO START URL', req.url)
-  res.send(indexStr)
+  let isLoggedOutPath = false
+  for (const re in loggedOutPaths) {
+    if (req.url.match(loggedOutPaths[re])) {
+      isLoggedOutPath = true
+      break
+    }
+  }
+  console.log('ELLO START URL', req.url, isLoggedOutPath)
+  if (isLoggedOutPath) {
+    renderFromServer(req, res)
+  } else {
+    res.send(indexStr)
+  }
 })
 
 const port = process.env.PORT || 6660
@@ -60,13 +121,13 @@ const start = (workerId) => {
       console.log(err)
       return
     }
-    console.log(`Worker ${workerId} listening at http://localhost:${port}`)
+    console.log('Worker ' + workerId + ' listening at http://localhost:' + port)
   })
 }
 
 if (workers > 1) {
   throng(start, {
-    workers,
+    workers: workers,
     lifetime: Infinity,
   })
 } else {
