8cfbda0ab036a826838ff5c1137e30bffcc6b366
Michael Westbom <michael@agilion.com>
2016-03-10 17:21:41 -0500

    Safer pathname access

--- src/middleware/requester.js
+++ src/middleware/requester.js
@@ -2,6 +2,7 @@
 import { camelizeKeys } from 'humps'
 import * as ACTION_TYPES from '../constants/action_types'
 import { resetAuth } from '../networking/auth'
+import { get } from 'lodash'
 
 const runningFetches = {}
 
@@ -158,7 +159,7 @@ export const requester = store => next => action => {
 
   const state = store.getState()
   // this allows us to set the proper result in the json reducer
-  payload.pathname = state.routing.location.pathname
+  payload.pathname = get(state, 'routing.location.pathname', '')
 
   // dispatch the start of the request
   store.dispatch({ type: REQUEST, payload, meta })
@@ -255,7 +256,7 @@ export const requester = store => next => action => {
                 delete runningFetches[error.response.url]
               }
               if ((error.response.status === 401 || error.response.status === 403) &&
-                  state.routing.location.pathname.indexOf('/onboarding') === 0 &&
+                  get(state, 'routing.location.pathname', '').indexOf('/onboarding') === 0 &&
                   typeof document !== 'undefined') {
                 resetAuth(store.dispatch, document.location)
               }
