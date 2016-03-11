5e33245cb4d769f658167f763bbe5a5be3ba3f1d
Michael Westbom <michael@agilion.com>
2016-03-11 16:08:12 -0500

    Resolve route TODOs

--- src/routes/post_detail/index.js
+++ src/routes/post_detail/index.js
@@ -3,10 +3,4 @@ export default {
   getComponents(location, cb) {
     cb(null, require('../../containers/details/PostDetail').default)
   },
-  // TODO: kill this whole onEnter before merging to master
-  // onEnter(nextState, replaceState, callback) {
-  //   if (callback) {
-  //     document.location.href = ENV.REDIRECT_URI + nextState.location.pathname
-  //   }
-  // },
 }
--- src/routes/user_detail/index.js
+++ src/routes/user_detail/index.js
@@ -1,27 +1,19 @@
-// TODO: uncomment this to lock down user paths
-// const TYPES = [
-//   'following',
-//   'followers',
-//   'loves',
-// ]
+const TYPES = [
+  'following',
+  'followers',
+  'loves',
+]
 
 export default {
   path: ':username(/:type)',
   getComponents(location, cb) {
     cb(null, require('../../containers/details/UserDetail').default)
   },
-  // TODO: kill this whole thing before merging to master
-  // onEnter(nextState, replaceState, callback) {
-  //   if (callback) {
-  //     document.location.href = ENV.REDIRECT_URI + nextState.location.pathname
-  //   }
-  // },
-  // TODO: uncomment this to lock down user paths
-  // onEnter(nextState, replaceState) {
-  //   const type = nextState.params.type
-  //   // redirect back to /username if type is unrecognized
-  //   if (type && TYPES.indexOf(type) === -1) {
-  //     replaceState(nextState, `/${nextState.params.username}`)
-  //   }
-  // },
+  onEnter(nextState, replace) {
+    const type = nextState.params.type
+    // redirect back to /username if type is unrecognized
+    if (type && TYPES.indexOf(type) === -1) {
+      replace({ pathname: `/${nextState.params.username}`, state: nextState })
+    }
+  },
 }
