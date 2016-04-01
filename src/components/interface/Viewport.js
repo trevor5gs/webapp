1f4e63e82d7eb6bad7131a3d755c35ded063e447
Matthew Kitt <mk@mkitt.net>
2016-04-01 05:02:58 -0600

    Move feature detection out of markup and into App
    This could potentially cause a FOUC but really the only thing the status of touch deals with are hover states.
    
    Getting rid of the `position: sticky` check too since it’s no longer in use.

--- src/components/interface/Viewport.js
+++ src/components/interface/Viewport.js
@@ -4,3 +4,16 @@ export const hideSoftKeyboard = () => {
   }
 }
 
+export const addFeatureDetection = () => {
+  const cl = document.documentElement.classList
+  if (!('ontouchstart' in document.documentElement)) {
+    cl.add('no-touch')
+  }
+  const onTouchStart = () => {
+    document.removeEventListener('touchstart', onTouchStart)
+    cl.remove('no-touch')
+    cl.add('has-touch')
+  }
+  document.addEventListener('touchstart', onTouchStart)
+}
+
