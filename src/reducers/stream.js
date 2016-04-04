9b6370e7406cc14ea1251b0758efe0e43f0e2d2c
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-04-04 09:48:05 -0600

    Fixes an issue where a comment couldn’t edit.
    * Allow stream to update when editing a comment
    * Fix a `setState` issue on block collection when submitting a comment
    
    [Fixes #116381579]

--- src/reducers/stream.js
+++ src/reducers/stream.js
@@ -4,12 +4,14 @@ export function stream(state = {}, action = { type: '' }) {
   if (action.type === AUTHENTICATION.LOGOUT || action.type === PROFILE.DELETE_SUCCESS) {
     return {}
   } else if (!(action.type === POST.DETAIL_FAILURE || action.type === PROFILE.DETAIL_FAILURE) &&
+             !(action.type.indexOf('COMMENT.') === 0 && action.type.indexOf('SUCCESS') > -1) &&
              action && action.meta && action.meta.updateResult === false) {
     return state
   } else if (action.type === POST.DETAIL_FAILURE ||
              action.type === PROFILE.DETAIL_FAILURE ||
              action.type.indexOf('LOAD_STREAM_') === 0 ||
              action.type.indexOf('LOAD_NEXT_CONTENT_') === 0 ||
+             (action.type.indexOf('COMMENT.') === 0 && action.type.indexOf('SUCCESS') > -1) ||
              (action.type.indexOf('POST.') === 0 && action.type.indexOf('SUCCESS') > -1)) {
     return {
       ...state,
