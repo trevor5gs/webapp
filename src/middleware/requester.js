15330caa031d210d4bac41395bc688cb4aa2f5cd
Jay Zeschin <jay.zeschin@modeset.com>
2016-04-20 11:34:14 -0600

    Default the port for local access token to 6660

--- src/middleware/requester.js
+++ src/middleware/requester.js
@@ -175,7 +175,7 @@ export const requester = store => next => action => {
     }
 
     const tokenPath = (typeof window === 'undefined') ?
-      `http://localhost:${process.env.PORT}/api/webapp-token` :
+      `http://localhost:${process.env.PORT || 6660}/api/webapp-token` :
       `${document.location.protocol}//${document.location.host}/api/webapp-token`
     return fetch(tokenPath, { credentials: 'same-origin' })
       .then((response) =>
