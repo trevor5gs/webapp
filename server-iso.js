d55a36b9fcd1a72b7ea8c1a8d0adfca855a3c51e
Jay Zeschin <jay.zeschin@modeset.com>
2016-04-20 11:49:34 -0600

    Send back a 500 if the prerender bombs out for any reason

--- server-iso.js
+++ server-iso.js
@@ -112,6 +112,7 @@ function renderFromServer(req, res) {
       // this will give you a js error like:
       // `window is not defined`
       console.log('ELLO CATCH ERROR', err)
+      res.status(500).end()
     })
   })
 }
