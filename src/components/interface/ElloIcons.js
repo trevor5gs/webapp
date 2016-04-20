55b2a900bf63cea5e9b4bb4436c97ff485789e8c
Matthew Kitt <mk@mkitt.net>
2016-04-20 13:23:45 -0600

    Update the stream loader and paginator animations
    Also fine tuned some of the positioning at various viewport dimensions
    
    [Finishes: #117951571]

--- src/components/interface/ElloIcons.js
+++ src/components/interface/ElloIcons.js
@@ -4,17 +4,17 @@ import { SVGBox } from './SVGComponents'
 
 // )
 const fortyPointSmilePath = () =>
-  <path d="M20.5 32c-5.5 0-10.2-3.7-11.6-9-.2-.6.2-1.2.8-1.4.6-.2 1.2.2 1.4.8 1.1 4.3 5 7.3 9.4 7.3s8.3-3 9.4-7.3c.2-.6.8-1 1.4-.8.6.2 1 .8.8 1.4-1.3 5.3-6.1 9-11.6 9z" />
+  <path className="SmilePath" d="M20.5 32c-5.5 0-10.2-3.7-11.6-9-.2-.6.2-1.2.8-1.4.6-.2 1.2.2 1.4.8 1.1 4.3 5 7.3 9.4 7.3s8.3-3 9.4-7.3c.2-.6.8-1 1.4-.8.6.2 1 .8.8 1.4-1.3 5.3-6.1 9-11.6 9z" />
 
 export const ElloMark = () =>
   <SVGBox className="ElloMark">
-    <path d="M0 0h40v40h-40z" />
+    <path className="FillPath" d="M0 0h40v40h-40z" />
     { fortyPointSmilePath() }
   </SVGBox>
 
 export const ElloBoxMark = () =>
   <SVGBox className="ElloBoxMark">
-    <path d="M0 0h40v40h-40z" />
+    <path className="FillPath" d="M0 0h40v40h-40z" />
     { fortyPointSmilePath() }
   </SVGBox>
 
