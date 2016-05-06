70dc8c3432f894e555bda6c09882d060d02d0620
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-05-06 16:07:37 -0600

    Add a comment to explain why the uid subtracts two

--- src/middleware/editor.js
+++ src/middleware/editor.js
@@ -30,6 +30,11 @@ export const editor = store => next => action => {
     const editorId = get(payload, 'editorId')
     const file = get(payload, 'file')
 
+    // The - 2 should always be consistent. The reason is that when a tmp image
+    // gets created at say uid 1 an additional text block is added to the bottom
+    // of the editor at uid 2 and the uid of the editor is now sitting at 3
+    // since it gets incremented after a block is added. So the - 2 gets us from
+    // the 3 back to the 1 where the image should reconcile back to.
     if (editorId && file) {
       store.dispatch(temporaryPostImageCreated(URL.createObjectURL(file), editorId))
       const uid = store.getState().editor[editorId].uid - 2
