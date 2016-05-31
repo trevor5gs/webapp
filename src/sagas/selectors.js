11814724de0f49779df0c2bb02c6bad37b7b71bd
Michael Westbom <michael@agilion.com>
2016-05-31 13:54:18 -0400

    Fix dumb typo

--- src/sagas/selectors.js
+++ src/sagas/selectors.js
@@ -1,5 +1,5 @@
 import { get } from 'lodash'
 
-export const isLoggedInSelector = state => get(state, 'authenticatoin.isLoggedIn')
+export const isLoggedInSelector = state => get(state, 'authentication.isLoggedIn')
 export const accessTokenSelector = state => get(state, 'authentication.accessToken')
 export const refreshTokenSelector = state => get(state, 'authentication.refreshToken')
