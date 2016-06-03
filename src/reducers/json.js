39401c64f25b3a32fdcdba3b3f6cde63e352beb4
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-06-03 15:48:27 -0600

    Instantly change the count and heart on love.
    [Fixes #120780379](https://www.pivotaltracker.com/story/show/120780379)

--- src/reducers/json.js
+++ src/reducers/json.js
@@ -377,6 +377,7 @@ export default function json(state = {}, action = { type: '' }) {
       return methods.deleteModel(state, newState, action, MAPPING_TYPES.POSTS)
     case ACTION_TYPES.POST.DETAIL_SUCCESS:
       return methods.updatePostDetail(newState, action)
+    case ACTION_TYPES.POST.LOVE_REQUEST:
     case ACTION_TYPES.POST.LOVE_SUCCESS:
     case ACTION_TYPES.POST.LOVE_FAILURE:
       return postMethods.updatePostLoves(state, newState, action)
