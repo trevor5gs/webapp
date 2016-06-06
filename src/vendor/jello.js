6abfb1f70ebf40066a51581c4c244304ebf8c057
Matthew Kitt <mk@mkitt.net>
2016-06-06 16:54:13 -0600

    Render Chrome font-smoothing w/ subpixel aliasing
    While I was in there I did a few cleanup things…
    
    - Use function declarations instead of function expressions
    - Add isChrome and isSafari
    - Add the isChrome class to the document when browsing with Chrome
    - Divide and sort functions based on order of importance
    
    [Finishes: #120778495](https://www.pivotaltracker.com/story/show/120778495)

--- src/vendor/jello.js
+++ src/vendor/jello.js
@@ -1,9 +1,55 @@
 let memoizedIsAndroid = undefined
+let memoizedIsChrome = undefined
+let memoizedIsElloAndroid = undefined
 let memoizedIsFirefox = undefined
 let memoizedIsIOS = undefined
-let memoizedIsElloAndroid = undefined
+let memoizedIsSafari = undefined
 
-export const addFeatureDetection = () => {
+export function isAndroid() {
+  if (typeof window === 'undefined') { return false }
+  if (typeof memoizedIsAndroid !== 'undefined') { return memoizedIsAndroid }
+  memoizedIsAndroid = /Android/gi.test(navigator.userAgent)
+  return memoizedIsAndroid
+}
+
+export function isChrome() {
+  if (typeof window === 'undefined') { return false }
+  if (typeof memoizedIsChrome !== 'undefined') { return memoizedIsChrome }
+  memoizedIsChrome = !!window.chrome && !!window.chrome.webstore
+  return memoizedIsChrome
+}
+
+export function isElloAndroid() {
+  if (typeof window === 'undefined') { return false }
+  if (typeof memoizedIsElloAndroid !== 'undefined') { return memoizedIsElloAndroid }
+  memoizedIsElloAndroid = /Ello Android/gi.test(navigator.userAgent)
+  return memoizedIsElloAndroid
+}
+
+export function isFirefox() {
+  if (typeof window === 'undefined') { return false }
+  if (typeof memoizedIsFirefox !== 'undefined') { return memoizedIsFirefox }
+  memoizedIsFirefox = /Firefox/gi.test(navigator.userAgent)
+  return memoizedIsFirefox
+}
+
+export function isSafari() {
+  if (typeof window === 'undefined') { return false }
+  if (typeof memoizedIsSafari !== 'undefined') { return memoizedIsSafari }
+  memoizedIsSafari = /Safari/gi.test(navigator.userAgent) && !isChrome()
+  return memoizedIsSafari
+}
+
+export function isIOS() {
+  if (typeof window === 'undefined') { return false }
+  if (typeof memoizedIsIOS !== 'undefined') { return memoizedIsIOS }
+  memoizedIsIOS = /iPad|iPhone|iPod/gi.test(navigator.userAgent)
+  return memoizedIsIOS
+}
+
+// -------------------------------------
+
+export function addFeatureDetection() {
   const cl = document.documentElement.classList
   if (!('ontouchstart' in document.documentElement)) {
     cl.add('no-touch')
@@ -13,40 +59,13 @@ export const addFeatureDetection = () => {
     cl.remove('no-touch')
     cl.add('has-touch')
   }
+  if (isChrome()) { cl.add('isChrome') }
   document.addEventListener('touchstart', onTouchStart)
 }
 
-export const hideSoftKeyboard = () => {
+export function hideSoftKeyboard() {
   if (document.activeElement) {
     document.activeElement.blur()
   }
-}
-
-export const isAndroid = () => {
-  if (typeof window === 'undefined') { return false }
-  if (typeof memoizedIsAndroid !== 'undefined') { return memoizedIsAndroid }
-  memoizedIsAndroid = /Android/gi.test(navigator.userAgent)
-  return memoizedIsAndroid
-}
-
-export const isElloAndroid = () => {
-  if (typeof window === 'undefined') { return false }
-  if (typeof memoizedIsElloAndroid !== 'undefined') { return memoizedIsElloAndroid }
-  memoizedIsElloAndroid = /Ello Android/gi.test(navigator.userAgent)
-  return memoizedIsElloAndroid
-}
-
-export const isFirefox = () => {
-  if (typeof window === 'undefined') { return false }
-  if (typeof memoizedIsFirefox !== 'undefined') { return memoizedIsFirefox }
-  memoizedIsFirefox = /Firefox/gi.test(navigator.userAgent)
-  return memoizedIsFirefox
-}
-
-export const isIOS = () => {
-  if (typeof window === 'undefined') { return false }
-  if (typeof memoizedIsIOS !== 'undefined') { return memoizedIsIOS }
-  memoizedIsIOS = /iPad|iPhone|iPod/gi.test(navigator.userAgent)
-  return memoizedIsIOS
 }
 
