9e6fb11dd1697cf138223886b0e7e27c7f876805
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-07-13 14:25:39 -0600

    Allow Android app to register on successful login.
    [#114906841](https://www.pivotaltracker.com/story/show/114906841)
    [#125322831](https://www.pivotaltracker.com/story/show/125322831)

--- src/sagas/push_subscription.js
+++ src/sagas/push_subscription.js
@@ -9,29 +9,31 @@ import {
 } from './selectors'
 
 export function* loginPushSubscribe() {
-  const action = yield take(PROFILE.REQUEST_PUSH_SUBSCRIPTION)
-  const { buildVersion, bundleId, marketingVersion, registrationId } = action.payload
-  if (yield select(isLoggedInSelector)) {
-    yield put(registerForGCM(registrationId, bundleId, marketingVersion, buildVersion))
-  } else {
-    yield take(AUTHENTICATION.USER_SUCCESS)
-    yield put(registerForGCM(registrationId, bundleId, marketingVersion, buildVersion))
+  while (true) {
+    const action = yield take(PROFILE.REQUEST_PUSH_SUBSCRIPTION)
+    const { buildVersion, bundleId, marketingVersion, registrationId } = action.payload
+    if (yield select(isLoggedInSelector)) {
+      yield put(registerForGCM(registrationId, bundleId, marketingVersion, buildVersion))
+    } else {
+      yield take(AUTHENTICATION.USER_SUCCESS)
+      yield put(registerForGCM(registrationId, bundleId, marketingVersion, buildVersion))
+    }
   }
 }
 
 export function* logoutPushUnsubscribe() {
-  yield take([AUTHENTICATION.LOGOUT, PROFILE.DELETE])
-  const registrationId = yield select(registrationIdSelector)
-  const bundleId = yield select(bundleIdSelector)
-  yield put(unregisterForGCM(registrationId, bundleId))
+  while (true) {
+    yield take([AUTHENTICATION.LOGOUT, PROFILE.DELETE])
+    const registrationId = yield select(registrationIdSelector)
+    const bundleId = yield select(bundleIdSelector)
+    yield put(unregisterForGCM(registrationId, bundleId))
+  }
 }
 
 export default function* pushSubscription() {
-  while (true) {
-    yield [
-      fork(loginPushSubscribe),
-      fork(logoutPushUnsubscribe),
-    ]
-  }
+  yield [
+    fork(loginPushSubscribe),
+    fork(logoutPushUnsubscribe),
+  ]
 }
 
