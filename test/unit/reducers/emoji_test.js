33851d31854115576c3ddde841e64497e27ad532
Matthew Kitt <mk@mkitt.net>
2016-07-19 13:56:37 -0600

    Add unit tests around the emoji reducer
    [#126628567](https://www.pivotaltracker.com/story/show/126628567)

---
+++ test/unit/reducers/emoji_test.js
@@ -0,0 +1,29 @@
+import { expect } from '../../spec_helper'
+import { emoji as reducer } from '../../../src/reducers/emoji'
+import { EDITOR } from '../../../src/constants/action_types'
+
+describe('emoji reducer', () => {
+  const emojis = [
+    { name: 'ello', imageUrl: 'https://ello.co/images/emoji/ello.png' },
+    { name: 'bread', imageUrl: 'https://ello.co/images/emoji/bread.png' },
+    { name: 'sparkles', imageUrl: 'https://ello.co/images/emoji/sparkles.png' },
+  ]
+
+  context('#initialState', () => {
+    it('sets up a default initialState', () => {
+      expect(reducer(undefined, {})).to.deep.equal({})
+    })
+  })
+
+  context('EDITOR', () => {
+    it('EDITOR.EMOJI_COMPLETER_SUCCESS makes it be emoji time', () => {
+      const action = {
+        type: EDITOR.EMOJI_COMPLETER_SUCCESS,
+        payload: { response: { emojis } },
+      }
+      const result = reducer(reducer, action)
+      expect(result).to.deep.equal({ emojis })
+    })
+  })
+})
+
