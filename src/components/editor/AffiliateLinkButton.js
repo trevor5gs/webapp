40cfd4307ef4f85eb8c262d16fa969d932979466
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-07-20 15:33:35 -0600

    Adds affiliate link event tracking.
    [#126697407](https://www.pivotaltracker.com/story/show/126697407)

--- src/components/editor/AffiliateLinkButton.js
+++ src/components/editor/AffiliateLinkButton.js
@@ -1,10 +1,16 @@
 import React, { PropTypes } from 'react'
 import { MoneyIcon } from './EditorIcons'
+import { dispatchTrackEvent } from '../../helpers/junk_drawer'
+
+function onAffiliateLinkButtonClick(e) {
+  dispatchTrackEvent('affililate_link_clicked', { link: e.target.href })
+}
 
 export const AffiliateLinkButton = ({ to }) =>
   <a
     className="AffiliateLinkButton"
     href={to}
+    onClick={onAffiliateLinkButtonClick}
     target="_blank"
   >
     <MoneyIcon />
