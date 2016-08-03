9c5a29b850cf64df55eab25752a23942d410738c
Matthew Kitt <mk@mkitt.net>
2016-08-02 23:58:51 -0600

    Modify the UserDetail title
    [#126717975](https://www.pivotaltracker.com/story/show/126717975)

--- src/components/helmets/UserDetailHelmet.js
+++ src/components/helmets/UserDetailHelmet.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react'
 import Helmet from 'react-helmet'
 
 export const UserDetailHelmet = ({ user }) => {
-  const title = `${user.username} | Ello`
+  const title = `(@${user.username}) | Ello`
   const image = user.coverImage && user.coverImage.optimized ? user.coverImage.optimized.url : null
   const description = `See ${user.username}'s work on Ello.`
   const robots = user.badForSeo ? 'noindex, follow' : 'index, follow'
