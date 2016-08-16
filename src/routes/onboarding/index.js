dbb0cd923bbafa79ac187dbab8d04e68aa60b9b4
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-08-16 17:14:15 -0600

    Delete the old onboarding containers.

--- src/routes/onboarding/index.js
+++ src/routes/onboarding/index.js
@@ -1,11 +1,6 @@
-import Communities from '../../containers/onboarding/Communities'
-import AwesomePeople from '../../containers/onboarding/AwesomePeople'
-import ProfileHeader from '../../containers/onboarding/ProfileHeader'
-import ProfileAvatar from '../../containers/onboarding/ProfileAvatar'
-import ProfileBio from '../../containers/onboarding/ProfileBio'
 import OnboardingCategoriesContainer from '../../containers/OnboardingCategoriesContainer'
-import OnboardingSettingsContainer from '../../containers/OnboardingSettingsContainer'
 import OnboardingInvitationsContainer from '../../containers/OnboardingInvitationsContainer'
+import OnboardingSettingsContainer from '../../containers/OnboardingSettingsContainer'
 
 export default [
   {
@@ -24,36 +19,6 @@ export default [
     path: 'onboarding/invitations',
     getComponent(location, cb) {
       cb(null, OnboardingInvitationsContainer)
-    },
-  },
-  {
-    path: 'onboarding/communities',
-    getComponent(location, cb) {
-      cb(null, Communities)
-    },
-  },
-  {
-    path: 'onboarding/awesome-people',
-    getComponent(location, cb) {
-      cb(null, AwesomePeople)
-    },
-  },
-  {
-    path: 'onboarding/profile-header',
-    getComponent(location, cb) {
-      cb(null, ProfileHeader)
-    },
-  },
-  {
-    path: 'onboarding/profile-avatar',
-    getComponent(location, cb) {
-      cb(null, ProfileAvatar)
-    },
-  },
-  {
-    path: 'onboarding/profile-bio',
-    getComponent(location, cb) {
-      cb(null, ProfileBio)
     },
   },
 ]
