17e03c17d9c208f11d19506d6d1fcbfc7ff67243
Matthew Kitt <mk@mkitt.net>
2016-08-30 01:46:47 -0600

    Hook into the API for image and cover presence
    [Finishes: #128102579](https://www.pivotaltracker.com/story/show/128102579)

--- src/selectors/profile.js
+++ src/selectors/profile.js
@@ -1,23 +1,29 @@
 import { createSelector } from 'reselect'
 import { get } from 'lodash'
 
+/* eslint-disable max-len */
 export const selectAvatar = (state) => get(state, 'profile.avatar')
 export const selectCoverImage = (state) => get(state, 'profile.coverImage')
 export const selectCreatedAt = (state) => get(state, 'profile.createdAt')
 export const selectExternalLinksList = (state) => get(state, 'profile.externalLinksList', [])
+export const selectHasAvatarPresent = (state) => get(state, 'profile.hasAvatarPresent', true)
+export const selectHasCoverImagePresent = (state) => get(state, 'profile.hasCoverImagePresent', true)
 export const selectId = (state) => get(state, 'profile.id')
 export const selectName = (state) => get(state, 'profile.name', '')
 export const selectShortBio = (state) => get(state, 'profile.shortBio', '')
 export const selectUsername = (state) => get(state, 'profile.username')
+/* eslint-enable max-len */
 
 // Memoized Selectors
 
 export const selectIsAvatarBlank = createSelector(
-  [selectAvatar], (avatar) => !(avatar && (avatar.tmp || avatar.original))
+  [selectHasAvatarPresent, selectAvatar], (hasAvatarPresent, avatar) =>
+    !hasAvatarPresent || !(avatar && (avatar.tmp || avatar.original))
 )
 
 export const selectIsCoverImageBlank = createSelector(
-  [selectCoverImage], (coverImage) => !(coverImage && (coverImage.tmp || coverImage.original))
+  [selectHasCoverImagePresent, selectCoverImage], (hasCoverImagePresent, coverImage) =>
+    !hasCoverImagePresent || !(coverImage && (coverImage.tmp || coverImage.original))
 )
 
 export const selectIsInfoFormBlank = createSelector(
