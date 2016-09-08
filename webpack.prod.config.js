8c4ec43f61dc8408968f0993b3405668f1a855b4
Matthew Kitt <mk@mkitt.net>
2016-09-08 03:35:50 -0600

    Add plugins and setup build/dev scripts
    The production builds are fully working here but the dev environment
    does. It has something to do with extract plugin and the html webpack
    plugin.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,13 +1,22 @@
-/* eslint-disable */
-var path = require('path')
-var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const autoprefixer = require('autoprefixer')
+const postcssApply = require('postcss-apply')
+const postcssCalc = require('postcss-calc')
+const postcssColorFunction = require('postcss-color-function')
+const postcssCustomMedia = require('postcss-custom-media')
+const postcssCustomProperties = require('postcss-custom-properties')
+const postcssImport = require('postcss-import')
+const postcssPxToRem = require('postcss-pxtorem')
+const postcssReporter = require('postcss-reporter')
+const postcssUrl = require('postcss-url')
+const pkg = require('./package.json')
 // load env vars first
 require('dotenv').load({ silent: process.env.NODE_ENV === 'production' })
 
-const nodeEnv = process.env.NODE_ENV || 'development'
+const nodeEnv = process.env.NODE_ENV || 'production'
 
 module.exports = {
   devtool: 'source-map',
@@ -15,23 +24,23 @@ module.exports = {
     main: './src/main',
   },
   output: {
-    path: path.join(__dirname, 'public/static'),
     filename: '[name].entry.js',
     chunkFilename: '[id].chunk.js',
     hash: true,
-    publicPath: (process.env.CDN || '') + '/static/',
+    path: path.join(__dirname, 'public/static'),
+    publicPath: `${(process.env.CDN || '')}/static/`,
   },
   plugins: [
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
-      ENV: JSON.stringify(require(path.join(__dirname, './env.js'))),
+      ENV: JSON.stringify(require(path.join(__dirname, './env.js'))), // eslint-disable-line
       'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
     }),
     new ExtractTextPlugin('bundle.css'),
     new HtmlWebpackPlugin({
       filename: '../index.html',
       chunks: ['main'],
-      hash:true,
+      hash: true,
       template: 'public/template.html',
       inject: 'body',
     }),
@@ -40,22 +49,37 @@ module.exports = {
       compress: {
         screw_ie8: true,
         warnings: false,
-      }
+      },
     }),
-    new webpack.optimize.OccurenceOrderPlugin(true)
+    new webpack.optimize.OccurenceOrderPlugin(true),
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      include: path.join(__dirname, 'src'),
-      exclude: /node_modules/,
-    },
-    {
-      test: /\.sass$/,
-      loader: ExtractTextPlugin.extract(
-          'css!autoprefixer!sass?indentedSyntax'
-      )
-    }]
-  }
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader', 'postcss-loader'),
+      },
+    ],
+  },
+  postcss(wp) {
+    return [
+      postcssImport({ addDependencyTo: wp }),
+      postcssUrl(),
+      postcssCustomProperties(),
+      postcssApply(),
+      postcssCalc(),
+      postcssColorFunction(),
+      postcssCustomMedia(),
+      postcssPxToRem({ propWhiteList: [], minPixelValue: 5 }),
+      autoprefixer({ browsers: pkg.browserlist }),
+      postcssReporter(),
+    ]
+  },
 }
+
