5c62e76005c6ff974b8c9449d293aacbcd56a340
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-09-19 15:12:16 -0600

    Fix a test with finding beacons in hero

--- test/unit/containers/HeroContainer_test.js
+++ test/unit/containers/HeroContainer_test.js
@@ -85,12 +85,14 @@ describe('HeroContainer', () => {
   context('#selectBroadcast', () => {
     it('selects with memoization whether the current route has a broadcast message', () => {
       let state = {
+        authentication: { isLoggedIn: true },
         gui: { lastDiscoverBeaconVersion: null },
         routing: { location: { pathname: '/discover', change: false } },
       }
       expect(selectBroadcast(state)).to.equal(DISCOVER.BEACON_TEXT)
 
       state = {
+        authentication: { isLoggedIn: true },
         gui: { lastDiscoverBeaconVersion: null },
         routing: { location: { pathname: '/discover', change: true } },
       }
@@ -98,30 +100,35 @@ describe('HeroContainer', () => {
       expect(selectBroadcast.recomputations()).to.equal(1)
 
       state = {
+        authentication: { isLoggedIn: true },
         gui: { lastDiscoverBeaconVersion: '1' },
         routing: { location: { pathname: '/discover', change: true } },
       }
       expect(selectBroadcast(state)).to.equal(null)
 
       state = {
+        authentication: { isLoggedIn: true },
         gui: { lastFollowingBeaconVersion: null },
         routing: { location: { pathname: '/following', change: true } },
       }
       expect(selectBroadcast(state)).to.equal(FOLLOWING.BEACON_TEXT)
 
       state = {
+        authentication: { isLoggedIn: true },
         gui: { lastFollowingBeaconVersion: '1' },
         routing: { location: { pathname: '/following', change: true } },
       }
       expect(selectBroadcast(state)).to.equal(null)
 
       state = {
+        authentication: { isLoggedIn: true },
         gui: { lastStarredBeaconVersion: null },
         routing: { location: { pathname: '/starred', change: true } },
       }
       expect(selectBroadcast(state)).to.equal(STARRED.BEACON_TEXT)
 
       state = {
+        authentication: { isLoggedIn: true },
         gui: { lastStarredBeaconVersion: '1' },
         routing: { location: { pathname: '/starred', change: true } },
       }
