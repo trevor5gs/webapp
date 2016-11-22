fdb89ae77e286126bf6804b1f80d6737014a7b4c
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2016-11-22 11:30:04 -0700

    Removes the category page title from discover
    [Finishes #134598955](https://www.pivotaltracker.com/story/show/134598955)

--- src/components/views/Discover.js
+++ src/components/views/Discover.js
@@ -3,16 +3,13 @@ import StreamContainer from '../../containers/StreamContainer'
 import { MainView } from '../views/MainView'
 
 export const Discover = ({
-  pageTitle,
   streamAction,
   }) =>
     <MainView className="Discover">
-      {pageTitle ? <h1 className="DiscoverPageTitle">{pageTitle}</h1> : null}
       <StreamContainer action={streamAction} />
     </MainView>
 
 Discover.propTypes = {
-  pageTitle: PropTypes.string,
   streamAction: PropTypes.object.isRequired,
 }
 
