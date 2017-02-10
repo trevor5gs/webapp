dd7fe953e05fe77a5c67a8da56a250775e35eae1
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-02-10 13:58:54 -0700

    Only import the pieces we need for this action

--- src/actions/stream.js
+++ src/actions/stream.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import * as ACTION_TYPES from '../constants/action_types'
-import * as MAPPING_TYPES from '../constants/mapping_types'
+import { LOAD_STREAM } from '../constants/action_types'
+import { POSTS } from '../constants/mapping_types'
 import { followingStream } from '../networking/api'
-import * as StreamRenderables from '../components/streams/StreamRenderables'
+import { postsAsGrid, postsAsList } from '../components/streams/StreamRenderables'
 import { ZeroFollowingStream } from '../components/zeros/Zeros'
 
 export function loadFollowing() {
   return {
-    type: ACTION_TYPES.LOAD_STREAM,
+    type: LOAD_STREAM,
     payload: { endpoint: followingStream() },
     meta: {
-      mappingType: MAPPING_TYPES.POSTS,
+      mappingType: POSTS,
       renderStream: {
-        asList: StreamRenderables.postsAsList,
-        asGrid: StreamRenderables.postsAsGrid,
+        asList: postsAsList,
+        asGrid: postsAsGrid,
         asZero: <ZeroFollowingStream />,
       },
     },
