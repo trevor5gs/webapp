dad9318933d79500431745960f086a42b7ab3fb3
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-02-10 10:41:45 -0700

    Removes the StarredContainer
    [Finishes #139510727](https://www.pivotaltracker.com/story/show/139510727)

--- src/routes/streams/index.js
+++ src/routes/streams/index.js
@@ -1,17 +1,10 @@
 import FollowingContainer from '../../containers/FollowingContainer'
-import StarredContainer from '../../containers/StarredContainer'
 
 export default [
   {
     path: 'following',
     getComponents(location, cb) {
       cb(null, FollowingContainer)
-    },
-  },
-  {
-    path: 'starred',
-    getComponents(location, cb) {
-      cb(null, StarredContainer)
     },
   },
 ]
