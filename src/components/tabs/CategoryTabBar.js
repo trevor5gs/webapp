d0fe17f913e51527c1648b51dab51dafe16c9aea
Matthew Kitt <mk@mkitt.net>
2017-02-10 07:22:29 -0700

    Remove code for the old categories

--- src/components/tabs/CategoryTabBar.js
+++ src/components/tabs/CategoryTabBar.js
@@ -61,30 +61,5 @@ export const CategoryTabBar = (props: TabBarProps) => {
   )
 }
 
-// import { TabListLinks } from './TabList'
-// export const CategoryTabBar = ({ pathname, tabs }) =>
-//   <div className="CategoryTabBar">
-//     <TabListLinks
-//       activePath={pathname}
-//       className="CategoryTabBarLinks"
-//       tabClasses="CategoryLabelTab"
-//       tabs={tabs}
-//     />
-//     <div className="CategoryTabBarUtility">
-//       <Link
-//         activeClassName="isActive"
-//         className="CategoryLabelTab"
-//         to="/discover/all"
-//       >
-//         See All
-//       </Link>
-//     </div>
-//   </div>
-
-// CategoryTabBar.propTypes = {
-//   pathname: PropTypes.string.isRequired,
-//   tabs: PropTypes.array.isRequired,
-// }
-
 export default CategoryTabBar
 
