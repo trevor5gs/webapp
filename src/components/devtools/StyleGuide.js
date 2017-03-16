69043fe748baf212294d0c5623671b9accb76ec0
Matthew Kitt <mk@mkitt.net>
2017-03-15 22:08:28 -0600

    Cleanup the styles in StyleGuide

--- src/components/devtools/StyleGuide.js
+++ src/components/devtools/StyleGuide.js
@@ -3,38 +3,36 @@ import React from 'react'
 import { css } from 'glamor'
 import { MainView } from '../views/MainView'
 import StyleGuideIcons from './StyleGuideIcons'
-import { flex, minBreak2, wrapperPaddingX } from '../../styles/cso'
+import { combine, flex, minBreak2, wrapperPaddingX } from '../../styles/cso'
 
-const headerStyle = {
-  ...wrapperPaddingX,
-  ...css({
+const headerStyle = combine(
+  wrapperPaddingX,
+  css({
     paddingTop: 20,
     [minBreak2]: {
       paddingTop: 40,
     },
   }),
-}
+)
 const h1Style = css({ fontSize: 24 })
-const navStyle = {
-  ...flex,
-  ...css({
+const navStyle = combine(
+  flex,
+  css({
     paddingTop: 20,
     paddingBottom: 20,
   }),
-}
-const buttonStyle = {
-  ...css({
-    fontSize: 18,
-    marginRight: 10,
-  }),
-}
+)
+const buttonStyle = css({
+  fontSize: 18,
+  marginRight: 10,
+})
 
 export default() =>
   <MainView className="StyleGuide">
-    <header {...headerStyle}>
-      <h1 {...h1Style}>Ello style guide</h1>
-      <nav {...navStyle} >
-        <button {...buttonStyle}>Icons</button>
+    <header className={headerStyle}>
+      <h1 className={h1Style}>Ello style guide</h1>
+      <nav className={navStyle} >
+        <button className={buttonStyle}>Icons</button>
       </nav>
     </header>
     <StyleGuideIcons />
