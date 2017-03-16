30305efadc188bceb4a7dad3b8d3399ac87c71d7
Matthew Kitt <mk@mkitt.net>
2017-03-15 20:17:04 -0600

    Simplify how we showcase and render icons

--- src/components/devtools/StyleGuideIcons.js
+++ src/components/devtools/StyleGuideIcons.js
@@ -46,39 +46,40 @@ import {
   TwitterIcon,
 } from '../assets/Icons'
 import {
+  combine,
   flex,
   flexColumn,
   flexWrap,
   wrapperPaddingX,
 } from '../../styles/cso'
 
-const sectionStyle = {
-  ...flex,
-  ...flexColumn,
-  ...flexWrap,
-  ...wrapperPaddingX,
-  ...css({
+const sectionStyle = combine(
+  flex,
+  flexColumn,
+  flexWrap,
+  wrapperPaddingX,
+  css({
     marginTop: 40,
     marginBottom: 40,
   }),
-}
+)
 
 const h2Style = css({
   marginBottom: 10,
   fontSize: 16,
 })
 
-const groupStyle = {
-  ...flex,
-  ...css({
+const groupStyle = combine(
+  flex,
+  css({
     marginBottom: 40,
     '> *': {
       marginRight: 10,
     },
   }),
-}
+)
 
-// Fix this
+// TODO: Fix this
 const badgeStyle = css({
   '& .CheckShape': {
     stroke: 'white',
@@ -86,124 +87,62 @@ const badgeStyle = css({
 })
 
 export default() =>
-  <section {...sectionStyle}>
-    <h2 {...h2Style}>Ello icons</h2>
-    <div {...groupStyle} >
+  <section className={sectionStyle}>
+    <h2 className={h2Style}>Ello icons</h2>
+    <div className={groupStyle}>
       <ElloBoxMark />
       <ElloMark />
       <ElloRainbowMark />
       <ElloDonutMark />
     </div>
-    <h2 {...h2Style}>Badge icons</h2>
-    <div {...groupStyle} >
-      <button {...badgeStyle} title="BadgeCheckIcon">
+    <h2 className={h2Style}>Badge icons</h2>
+    <div className={groupStyle}>
+      <button className={badgeStyle} title="BadgeCheckIcon">
         <BadgeCheckIcon />
       </button>
     </div>
-    <h2 {...h2Style}>Icons</h2>
-    <div {...groupStyle} >
-      <button title="ArrowEastIcon">
-        <ArrowEastIcon />
-      </button>
-      <button title="BoltIcon">
-        <BoltIcon />
-      </button>
-      <button title="BubbleIcon">
-        <BubbleIcon />
-      </button>
-      <button title="CheckIcon">
-        <CheckIcon />
-      </button>
-      <button title="CheckIconHeader">
-        <CheckIconHeader />
-      </button>
-      <button title="CheckIconMini">
-        <CheckIconMini />
-      </button>
-      <button title="ChevronIcon">
-        <ChevronIcon />
-      </button>
-      <button title="DotsIcon">
-        <DotsIcon />
-      </button>
-      <button title="EyeIcon">
-        <EyeIcon />
-      </button>
-      <button title="FailureIcon">
-        <FailureIcon />
-      </button>
-      <button title="FlagIcon">
-        <FlagIcon />
-      </button>
-      <button title="GridIcon">
-        <GridIcon />
-      </button>
-      <button title="HeartIcon">
-        <HeartIcon />
-      </button>
-      <button title="ListIcon">
-        <ListIcon />
-      </button>
-      <button title="MarkerIcon">
-        <MarkerIcon />
-      </button>
-      <button title="PencilIcon">
-        <PencilIcon />
-      </button>
-      <button title="PlusIconHeader">
-        <PlusIconHeader />
-      </button>
-      <button title="PlusIconMini">
-        <PlusIconMini />
-      </button>
-      <button title="ReplyIcon">
-        <ReplyIcon />
-      </button>
-      <button title="RepostIcon">
-        <RepostIcon />
-      </button>
-      <button title="RequestIcon">
-        <RequestIcon />
-      </button>
-      <button title="ShareIcon">
-        <ShareIcon />
-      </button>
-      <button title="SuccessIcon">
-        <SuccessIcon />
-      </button>
-      <button title="XBoxIcon">
-        <XBoxIcon />
-      </button>
-      <button title="XIcon">
-        <XIcon />
-      </button>
+    <h2 className={h2Style}>Icons</h2>
+    <div className={groupStyle}>
+      { [
+        ArrowEastIcon,
+        BoltIcon,
+        BubbleIcon,
+        CheckIcon,
+        CheckIconHeader,
+        CheckIconMini,
+        ChevronIcon,
+        DotsIcon,
+        EyeIcon,
+        FailureIcon,
+        FlagIcon,
+        GridIcon,
+        HeartIcon,
+        ListIcon,
+        MarkerIcon,
+        PencilIcon,
+        PlusIconHeader,
+        PlusIconMini,
+        ReplyIcon,
+        RepostIcon,
+        RequestIcon,
+        ShareIcon,
+        SuccessIcon,
+        XBoxIcon,
+        XIcon,
+      ].map(icon => <button key={icon.name} title={icon.name}>{icon()}</button>) }
     </div>
-    <h2 {...h2Style}>Social icons</h2>
-    <div {...groupStyle} >
-      <button title="MailIcon">
-        <MailIcon />
-      </button>
-      <button title="FacebookIcon">
-        <FacebookIcon />
-      </button>
-      <button title="TwitterIcon">
-        <TwitterIcon />
-      </button>
-      <button title="PinterestIcon">
-        <PinterestIcon />
-      </button>
-      <button title="GooglePlusIcon">
-        <GooglePlusIcon />
-      </button>
-      <button title="TumblrIcon">
-        <TumblrIcon />
-      </button>
-      <button title="RedditIcon">
-        <RedditIcon />
-      </button>
-      <button title="LinkedInIcon">
-        <LinkedInIcon />
-      </button>
+    <h2 className={h2Style}>Social icons</h2>
+    <div className={groupStyle} >
+      { [
+        FacebookIcon,
+        GooglePlusIcon,
+        LinkedInIcon,
+        MailIcon,
+        PinterestIcon,
+        RedditIcon,
+        TumblrIcon,
+        TwitterIcon,
+      ].map(icon => <button key={icon.name} title={icon.name}>{icon()}</button>) }
     </div>
   </section>
 
